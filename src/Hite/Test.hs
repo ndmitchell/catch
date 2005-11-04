2f99ae40d07ca00bdaaedf2ec73af973b48361a4
ndm <unknown>
2005-11-04 17:52:21 +0000

    Add a test for Forward

--- src/Hite/Test.hs
+++ src/Hite/Test.hs
@@ -8,6 +8,7 @@ import Hite.Read
 import Hite.Show
 import Hite.Check
 import Hite.Reachable
+import Hite.Forward
 
 
 readAndParse :: FilePath -> IO Hite
@@ -23,3 +24,17 @@ testRead file = do h <- readAndParse file
 testCheck :: FilePath -> IO ()
 testCheck file = do h <- readAndParse file
                     print $ check h
+
+testReachable :: FilePath -> String -> IO ()
+testReachable file main = do h <- readAndParse file
+                             let m = if null main then "main" else main
+                             print $ reachable main h
+
+testForward :: FilePath -> IO ()
+testForward = testFilter forward
+
+
+
+testFilter :: (Hite -> Hite) -> FilePath -> IO ()
+testFilter f file = do h <- readAndParse file
+                       print $ f h
