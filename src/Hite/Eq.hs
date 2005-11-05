02f7665787c51c26c947ebddcbcc5af000ae9583
ndm <unknown>
2005-11-05 15:56:28 +0000

    Add some level of equality test

--- src/Hite/Eq.hs
+++ src/Hite/Eq.hs
@@ -3,7 +3,18 @@
 module Hite.Eq() where
 
 import Hite.Type
+import List
+
+
+compareUnordered :: (Eq a, Ord b) => (a -> b) -> [a] -> [a] -> Bool
+compareUnordered f xs ys =
+        (length xs == length ys) &&
+        and (zipWith (==) (sortBy g xs) (sortBy g ys))
+    where
+        g a b = f a `compare` f b
+
 
 instance Eq Hite where
-    a == b = False
-    
+    (Hite da fa) == (Hite db fb) = 
+        compareUnordered dataName da db &&
+        compareUnordered funcName fa fb
