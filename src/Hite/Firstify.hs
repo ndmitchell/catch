b776c3ddae16019f34713e67e67ac070525ec32e
ndm <unknown>
2005-11-05 18:51:13 +0000

    Completely rewritten firstification code

--- src/Hite/Firstify.hs
+++ src/Hite/Firstify.hs
@@ -1,6 +1,87 @@
 
 module Hite.Firstify(firstify) where
 
+import Hite.Type
+import List
+import General
+
+
+firstify :: Hite -> Hite
+firstify hite = foldl specialise hite $ filter (canSpecialise hite) $ getHigherOrder hite
+
+
+specialise :: Hite -> (FuncName, FuncArg) -> Hite
+specialise hite (name, arg) = hite{funcs = oldfuncs ++ newfuncs}
+    where
+        pos = getArgPos name arg hite
+        (Func _ funcArgs funcBody) = getFunc name hite
+        
+        
+        oldfuncs = map (\x -> x{expr = mapExpr bind (expr x)}) (funcs hite)
+        bind c@(Call _ args) | ar /= [] && (arr `elem` useful) =
+            Call (CallFunc $ genName arr) (remArg args)
+            where
+                ar = getArgVar c
+                arr = head ar
+        bind x = x
+        
+        
+        newfuncs = map newfunc useful
+        newfunc replace = Func newName (remArg funcArgs) (mapExpr f funcBody)
+            where
+                newName = genName replace
+            
+                f c@(Call _ args) | getArgVar c == [replace] =
+                    Call (CallFunc newName) (remArg args)
+                f (Var x []) | x == arg = CallFunc replace
+                f x = x
+                                                    
+        
+        useful = nub [argVal | Func func _ expr <- funcs hite,
+                               func /= name,
+                               call <- allExpr expr,
+                               argVal <- getArgVar call]
+
+        getArgVar :: Expr -> [String]
+        getArgVar c@(Call (CallFunc nam) args) |
+            nam == name = 
+            case callArg c pos of
+                 Just (CallFunc n) -> [n]
+                 _ -> []
+        getArgVar _ = []
+        
+        remArg args = take (pos-1) args ++ drop pos args
+        genName x = name ++ "_FIRST_" ++ x
+                                                      
+
+
+-- get a list of all higher order functions
+getHigherOrder :: Hite -> [(FuncName, FuncArg)]
+getHigherOrder hite = nub [(funcName f, arg) | f <- funcs hite, Call (Var arg []) _ <- allExpr $ expr f]
+
+
+canSpecialise :: Hite -> (FuncName, FuncArg) -> Bool
+canSpecialise hite (name, arg) = sum (map f $ allExpr $ expr $ getFunc name hite) == 0
+    where
+        pos = getArgPos name arg hite
+        
+        f (Var n path) | n == arg = 0+1
+        f x@(Call (CallFunc func) args) | name == func &&
+                                        callArg x pos == Just (Var arg []) = 0-1
+        f (Call (Var n []) _) | n == arg = 0-1
+        f _ = 0
+        
+
+recursiveCalls :: Hite -> FuncName -> [Expr]
+recursiveCalls hite func = [x |
+    x@(Call (CallFunc name) args) <- allExpr $ expr $ getFunc func hite,
+    name == func]
+
+
+{-
+
+
+
 
 import Hite
 import List
@@ -91,3 +172,5 @@ getParams hite = concatMap f (funcs hite)
                 f x@(Call (CallFunc a) _) = a == name
                 f _ = False
 
+
+-}
