8c11b208cc26e7935cb6b578481c68efe1f56569
ndm <unknown>
2006-01-04 17:08:15 +0000

    Remove magic underscore on left of case

--- src/Convert/CoreHite.hs
+++ src/Convert/CoreHite.hs
@@ -4,6 +4,8 @@ module Convert.CoreHite(coreHite) where
 import Core
 import Hite
 
+import List
+
 
 baseTypes :: [Data]
 baseTypes = [Data "[]" [Ctor "[]" [], Ctor ":" ["hd","tl"]]]
@@ -29,23 +31,34 @@ convExpr types subs x = f subs x
         f subs y = case y of
                 CoreCon x -> Make x []
                 CoreApp x xs -> Call (f subs x) (map (f subs) xs)
-                CoreInt x -> CallFunc "prim.int"
-                CoreChr x -> CallFunc "prim.chr"
-                CoreStr x -> CallFunc "prim.str"
+                CoreInt x -> CallFunc "prim_int"
+                CoreChr x -> CallFunc "prim_chr"
+                CoreStr x -> CallFunc "prim_str"
                 
                 CoreVar x -> rep x
                     
-                CoreCase _ alts -> Case rSwitch (map g alts)
+                CoreCase _ _ -> Case rSwitch (concatMap g alts)
             where
                 rep x = case lookup x subs of
                             Just a -> a
                             Nothing -> CallFunc x
                 
-                CoreCase (CoreVar switch) _ = y
+                CoreCase (CoreVar switch) alts = y
                 rSwitch = rep switch
                 
-                g (CoreVar "_", x) = ("_", f subs x)
-                g (CoreApp (CoreCon con) args, x) = (con, f (zipWith h [0..] args ++ subs) x)
+                g (CoreVar "_", x) = map (\a -> (a, body)) (allCtors \\ ctorNames)
+                    where
+                        body = f subs x
+                        
+                        ctorNames = concatMap (h . fst) alts
+                        h (CoreApp (CoreCon x) _) = [x]
+                        h _ = []
+                    
+                        allCtors = map ctorName $ ctors $
+                            getDataFromCtor (head ctorNames) (Hite types [])
+                        
+                
+                g (CoreApp (CoreCon con) args, x) = [(con, f (zipWith h [0..] args ++ subs) x)]
                     where h n (CoreVar arg) = (arg, Sel rSwitch (getCtor con n))
 
                 getCtor con n = head [xs !! n | Data _ cs <- types, Ctor c xs <- cs, c == con]
