8dd768918ccdb09082970f8f201b27db985e138c
ndm <unknown>
2006-01-04 17:12:12 +0000

    Add support for -cores

--- src/Core.hs
+++ src/Core.hs
@@ -18,5 +18,14 @@ import CmdLineData
 
 
 cmdLine = [
-            CmdLine "core" OptCore OptCore (const return) "Reads in a core file"
+            CmdLine "core" OptCore OptCore (const return) "Reads in a core file",
+            CmdLine "cores" OptInputs OptCore (const f) "Reads in many core files"
           ]
+    where
+        f (DatInputs xs) =
+            do
+                srcs <- mapM readFile xs
+                let inps = concatMap ((\(Core a) -> a) . readCore) srcs
+                return $ DatCore $ Core inps
+
+                               
