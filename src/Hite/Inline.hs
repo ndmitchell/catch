989484075428b12b581472b9e808f5d52ee8c976
ndm <unknown>
2006-01-06 03:15:28 +0000

    Write a basic inline function

--- src/Hite/Inline.hs
+++ src/Hite/Inline.hs
@@ -5,4 +5,24 @@ import Hite.Type
 
 
 inline :: Hite -> Hite
-inline h = h
+inline hite = mapExpr f hite
+    where
+        res = inlineable hite
+        
+        f (CallFunc x) = case lookup x res of
+                             Just k -> CallFunc k
+                             Nothing -> CallFunc x
+        f x = x
+
+
+
+inlineable :: Hite -> [(FuncName, FuncName)]
+inlineable hite = concatMap f (funcs hite)
+    where
+        f (Func name args1 (Call (CallFunc x) args2) _) | and (zipWith g args1 args2) = [(name, x)]
+        f _ = []
+        
+        g s1 (Var s2 _) = s1 == s2
+        g _ _ = False
+
+
