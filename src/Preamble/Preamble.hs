81f0bde653b6279a59468d4b8cb585eb33757353
ndm <unknown>
2006-01-06 14:13:13 +0000

    Add some stuff to the preamble, more like a prelude now

--- src/Preamble/Preamble.hs
+++ src/Preamble/Preamble.hs
@@ -1,6 +1,16 @@
 
 module Preamble where
 
+import Prelude hiding (
+    -- standard
+    map, head, (.)
+    )
+
+
+-- SPECIAL FUNCTIONS
+
+-- yhc bottoms out if some of these are not imported
+
 preamble_undefined = preamble_undefined
 
 _apply1 f x = f x
@@ -13,5 +23,12 @@ preamble_error msg = case preamble_error_empty_list of
 
 prim_STRING x = x
 prim_str = undefined
+
+
+-- STANDARD FUNCTIONS
+head (x:xs) = x
+
+map f [] = []
+map f (x:xs) = f x : map f xs
 
 
