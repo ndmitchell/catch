cecd6156ec78be097eecc9b7b7d06ecc8606dd99
ndm <unknown>
2006-01-08 18:28:29 +0000

    Add Pred.Simplify

--- src/Constraint.hs
+++ src/Constraint.hs
@@ -1,10 +1,11 @@
 
 module Constraint(
     module Constraint.Req, module Pred.Type, module Constraint.Constraint,
-    module RegExp
+    module RegExp, module Pred.Simplify
     ) where
 
 import Constraint.Req
 import Pred.Type
+import Pred.Simplify
 import RegExp
 import Constraint.Constraint
