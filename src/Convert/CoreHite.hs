5dbe93e9195b74af8135fda65099d50f1edd6293
ndm <unknown>
2006-01-09 05:06:12 +0000

    Convert let expressions out

--- src/Convert/CoreHite.hs
+++ src/Convert/CoreHite.hs
@@ -14,14 +14,40 @@ coreHite (Core xs) = Hite [] (concatMap convFunc xs)
 getName (CoreVar x) = x
 
 
+
+allReplace :: [(CoreExpr, CoreExpr)] -> CoreExpr -> CoreExpr
+allReplace ren x = mapCore f x
+    where
+        f x = case lookup x ren of
+                  Nothing -> x
+                  Just a -> a
+
+
+
+letExpand :: CoreExpr -> CoreExpr
+letExpand x = mapCore f x
+    where
+        f (CoreLet (x:xs) y) = f $ allReplace [g x] (CoreLet xs y)
+        f (CoreLet []     y) = y
+        
+--        error $ show (map g x) ++ "\n\n" ++ showCoreExpr y ++ "\n\n" ++ showCoreExpr (allReplace (map g x) y)
+        f x = x
+        
+        g (CoreFunc (CoreApp x []) y) = (x,y)
+
+
+
+
+
 convFunc :: CoreFunc -> [Func]
-convFunc (CoreFunc (CoreApp orig_name orig_args) body) =
+convFunc (CoreFunc (CoreApp orig_name orig_args) bad_body) =
         if null complexCases then
             [Func name args (convExpr ren body) Star]
         else
             [Func name args newBody Star,
              Func newName allArgs (convExpr ren2 body2) Star]
     where
+        body = letExpand bad_body
         name = getName orig_name
         args = map getName orig_args
         newName = name ++ "_CASE"
@@ -32,7 +58,7 @@ convFunc (CoreFunc (CoreApp orig_name orig_args) body) =
         ren =  [(x, Var x "") | x <- args]
         ren2 = [(x, Var x "") | x <- allArgs]
         
-        body2 = mapCore h body
+        body2 = allReplace (zip complexCases (map CoreVar newArgs)) body
         
         newArgs = ["case_" ++ show n | n <- [1..length complexCases]]
         allArgs = args ++ newArgs
@@ -41,12 +67,7 @@ convFunc (CoreFunc (CoreApp orig_name orig_args) body) =
         g (CoreCase (CoreVar _) _) = []
         g (CoreCase x _) = [x]
         g _ = []
-        
-        renComplex = zip complexCases newArgs 
-        
-        h x@(CoreCase (CoreVar _) _) = x
-        h (CoreCase x alts) = CoreCase (CoreVar $ fromJust $ lookup x renComplex) alts
-        h x = x
+
 
 
 convExpr :: [(String, Expr)] -> CoreExpr -> Expr
@@ -63,6 +84,8 @@ convExpr subs x = f subs x
                 CoreVar x -> rep x
                     
                 CoreCase _ _ -> Case rSwitch (map g alts)
+                
+                x -> error $ "Convert.CoreHite.convExpr: " ++ show x
             where
                 rep x = case lookup x subs of
                             Just a -> a
@@ -74,5 +97,8 @@ convExpr subs x = f subs x
                 g (CoreVar "_", x) = ("_", f subs x)
                 g (CoreApp (CoreCon con) args, x) = (con, f (zipWith h [0..] args ++ subs) x)
                     where h n (CoreVar arg) = (arg, Sel rSwitch (getCtor con n))
+                g (CoreCon con, x) = g (CoreApp (CoreCon con) [], x)
+                   
+                g x = error $ "Convert.CoreHite.g: " ++ show x
 
                 getCtor con n = con ++ "$" ++ show n
