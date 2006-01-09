0cd7f9b598d361679fe5147f7724a046344ea8fd
ndm <unknown>
2006-01-09 01:36:58 +0000

    Have complex propogations, which are case statement dependant

--- src/Checker/Propagate.hs
+++ src/Checker/Propagate.hs
@@ -4,21 +4,35 @@ module Checker.Propagate(propagate) where
 
 import Hite
 import Constraint
+import List
 
 
 propagate :: Hite -> Req -> Reqs
 propagate hite (Req (Var arg name) path set) = 
-        predAnd $ concatMap f $ allExpr $ callOne hite
+        predAnd $ concatMap (f predFalse . body) $ funcs $ callOne hite
     where
         pos = getArgPos name arg hite
-
-        f c@(Call (CallFunc n) _) | n == name = 
+        
+        
+        f :: Reqs -> Expr -> [Reqs]
+        f hist c@(Call (CallFunc n) args) | n == name =
+                concatMap (f hist) args ++
                 case callArg c pos of
-                    Nothing -> [predFalse ] {- $ "Unsaturated use of partial function, " ++ name ++
-                                           " (wanted " ++ show pos ++ ")"] -}
-                    Just x -> [predLit $ Req x path set]
+                    Nothing -> [predOr [hist, predFalse]] -- unsaturued use of partial app
+                    Just x -> [predOr [hist, predLit $ Req x path set]]
+                    
+        f hist (Case on alts) = concatMap g alts
+            where
+                allCtor = map ctorName $ ctors $ getDataFromCtor (fst $ head alts) hite
+                g (typ, expr) = f (predOr [hist, predLit $ Req on regLambda (allCtor \\ [typ])]) expr
+                
+        
+        f hist x = concatMap (f hist) $ case x of
+            Call x xs -> x : xs
+            Make _ xs -> xs
+            Sel x _ -> [x]
+            _ -> []
 
-        f _ = []
 
 
 propagate hite req = error $ "Internal error, complex pattern as subject of req: " ++ show req
