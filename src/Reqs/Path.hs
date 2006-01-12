a183ba0bce1c5ef5f43bbd094b5dd93d514e4db4
ndm <unknown>
2006-01-12 18:01:47 +0000

    Move to a more abstract path type

--- src/Reqs/Path.hs
+++ src/Reqs/Path.hs
@@ -1,33 +1,6 @@
 
 module Reqs.Path(
-    Path,
-    pathIntegrate,
-    pathQuotient,
-    pathBlur,
-    pathLambda,
-    pathIsEmpty,
-    pathIsEwp
+    module Reqs.PathRegExp
     ) where
 
-import RegExp.Type
-import RegExp.Parse
-import RegExp.Prop
-import RegExp.Blur
-import RegExp.Simplify
-
-
-type Path a = RegExp a
-
-
-pathIntegrate a b = regConcat [regLit a, b]
-
-pathQuotient x = quotient x
-
-pathBlur x = blur x
-
-pathLambda :: (Eq a, Show a) => Path a
-pathLambda = regLambda
-
-pathIsEmpty x = isEmpty x
-
-pathIsEwp x = isEwp x
+import Reqs.PathRegExp
---
+++ src/Reqs/PathRegExp.hs
@@ -0,0 +1,33 @@
+
+module Reqs.PathRegExp(
+    Path,
+    pathIntegrate,
+    pathQuotient,
+    pathBlur,
+    pathLambda,
+    pathIsEmpty,
+    pathIsEwp
+    ) where
+
+import RegExp.Type
+import RegExp.Parse
+import RegExp.Prop
+import RegExp.Blur
+import RegExp.Simplify
+
+
+type Path a = RegExp a
+
+
+pathIntegrate a b = regConcat [regLit a, b]
+
+pathQuotient x = quotient x
+
+pathBlur x = blur x
+
+pathLambda :: (Eq a, Show a) => Path a
+pathLambda = regLambda
+
+pathIsEmpty x = isEmpty x
+
+pathIsEwp x = isEwp x
