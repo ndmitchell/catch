fe1a310b99fda1357c7cfbe8cb6b7cb7f4802195
ndm <unknown>
2006-01-26 17:13:49 +0000

    Initial version, with a pretty output

---
+++ src/Pred/Show.hs
@@ -0,0 +1,49 @@
+
+module Pred.Show where
+
+import Pred.Type
+import List
+
+instance Show a => Show (Pred a) where
+    show x = showPred x
+    
+
+showPred :: Show a => Pred a -> String
+showPred x = showPredBy show x
+
+prettyPred :: Show a => Pred a -> String
+prettyPred x = prettyPredBy show x
+
+
+showPredBy :: (a -> String) -> Pred a -> String
+showPredBy f x =
+    case x of
+        PredOr  [] -> "False"
+        PredAnd [] -> "True"
+        PredLit  a -> f a
+        PredOr  xs -> disp 'v' xs
+        PredAnd xs -> disp '^' xs
+    where
+        disp sym xs = "(" ++ mid ++ ")"
+            where mid = concat $ intersperse [' ',sym,' '] $ map (showPredBy f) xs
+
+
+prettyPredBy :: (a -> String) -> Pred a -> String
+prettyPredBy showItem x = unlines $ f x
+    where
+        f x = case x of
+            PredOr  [] -> ["False"]
+            PredAnd [] -> ["True"]
+            PredLit  a -> [showItem a]
+            PredOr  xs -> g 'v' xs
+            PredAnd xs -> g '^' xs
+
+        g sym xs | all isAtom xs = ["(" ++ (concat $ intersperse [' ',sym,' '] $ map (head . f) xs) ++ ")"]
+                 | otherwise = ["("] ++ (map indent $ concat $ intersperse [[sym]] $ map f xs) ++ [")"]
+        
+        indent x = "  " ++ x
+        
+        isAtom (PredOr  []) = True
+        isAtom (PredAnd []) = True
+        isAtom (PredLit _ ) = True
+        isAtom _ = False
