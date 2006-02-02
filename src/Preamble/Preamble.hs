dac55250e429707a0f0531e9186169205bf79266
ndm <unknown>
2006-02-02 18:29:06 +0000

    Rewrite of the Preamble, more principled, less hacky Rewrite of the Preamble, more principled, less hacky Also contains a lot more features

--- src/Preamble/Preamble.hs
+++ src/Preamble/Preamble.hs
@@ -1,35 +1,87 @@
 
 module Preamble where
 
+{-
+GENERAL NOTES:
+
+Yhc requires certain bits of the prelude to be available,
+so importing Prelude() means it won't compile. However qualified Prelude
+seems to get most of the way there.
+
+Some things however require a non-qualified import, such as [] and Bool.
+-}
+
+
+-- special imports
 import qualified Prelude
 import Prelude( []((:),[]) , Bool(True,False) )
 
 
--- SPECIAL FUNCTIONS
 
--- yhc bottoms out if some of these are not imported
-
-undefined = undefined
+---------------------------------------------------------------------
+-- YHC.Internal
+--
+-- Special functions, definitions copied directly
 
 _apply1 f x = f x
+_apply2 f x y = f x y
+_apply3 f x y z = f x y z
+_apply4 f x y z u = f x y z u
+_id x = x
 
 
-preamble_error_empty_list = []
-preamble_error msg = case preamble_error_empty_list of
-    (_:_) -> undefined
+
+---------------------------------------------------------------------
+-- Special
+--
+-- Functions which are very Yhc/Catch specific
+
+-- catch_any is desugared to _, which is all possible values
+-- used to encode non-determinism
+-- treated specially by the checker
+catch_any = catch_any
 
 
 prim_STRING x = x
-prim_str = undefined
+prim_str = catch_any
 
 
--- STANDARD FUNCTIONS
+
+---------------------------------------------------------------------
+-- Prelude
+--
+-- Special functions, which have a different semantics in CATCH
+
+error msg = case [] of
+    (_:_) -> catch_any
+
+
+---------------------------------------------------------------------
+-- Prelude.General
+--
+-- Just general small functions
+
+flip f x y  = f y x
+
+
+
+---------------------------------------------------------------------
+-- Prelude.List
+--
+-- the List constructor is considered to be internal, and is special cased
+-- it really doesn't seem possible to write a custom one for MANY reasons
+
+--   Preamble     [] 
+data Preamble_Hex_5B5D a = Preamble_Hex_5B5D | Preamble_Hex_58 {hd :: a, tl :: [a]}
+
+
 head (x:xs) = x
 
 map f [] = []
 map f (x:xs) = f x : map f xs
 
-
+-- Hugs implementation that I don't understand :)
+-- reverse = foldl (flip (:)) []
 reverse xs = reverse_acc xs []
     where
         reverse_acc [] ys = ys
@@ -42,67 +94,70 @@ null [] = True
 null _ = False
 
 
-True || _ = True
+
+---------------------------------------------------------------------
+-- Prelude.Bool
+--
+-- everything to do with booleans
+
+True  || _ = True
 False || a = a
 
-flip f x y  = f y x
+True  && a = a
+False && _ = False
+
+not True = False
+not False = True
 
 
 
-class Eq a  where
+---------------------------------------------------------------------
+-- Prelude.Comparison
+--
+-- Eq and Ord declarations
+-- if its Eq directly, then I can't use deriving in this module
+
+data Ordering = LT | EQ | GT
+                deriving Prelude.Eq
+
+class Preamble_Eq a  where
     (==), (/=)      :: a -> a -> Bool
 
     x /= y      =  not (x == y)
     x == y      =  not (x /= y)
 
 
-not True = False
-not False = True
+class  (Preamble_Eq a) => Preamble_Ord a  where
+    compare :: a -> a -> Ordering
+    (<), (<=), (>=), (>) :: a -> a -> Bool
+    max, min :: a -> a -> a
 
+    compare x y
+      | x == y    = EQ
+      | x <= y    = LT
+      | True      = GT
 
-class  (Eq a) => Ord a  where
-    compare     :: a -> a -> Ordering
-    (<), (<=), (>=), (>):: a -> a -> Bool
-    max, min        :: a -> a -> a
+    x <= y      = (Prelude./=) (compare x y) GT
+    x <  y      = (Prelude.==) (compare x y) LT
+    x >= y      = (Prelude./=) (compare x y) LT
+    x >  y      = (Prelude.==) (compare x y) GT
 
-    compare x y = case x == y of
-                    True -> EQ
-                    False -> case x <= y of
-                            True -> LT
-                            False -> GT
-
-    x <= y      = compare x y /= GT
-    x <  y      = compare x y == LT
-    x >= y      = compare x y /= LT
-    x >  y      = compare x y == GT
-
-    max x y = case x >= y of
-                True -> y
-                False -> x
-
-    min x y = case x >= y of
-                True -> x
-                False -> y
+    max x y | x >= y = x
+            | True   = y
+    min x y | x <= y = x
+            | True   = y
 
 
 
-data Ordering = LT | EQ | GT
-
-
-instance Eq Ordering where
-    LT == LT = True
-    EQ == EQ = True
-    GT == GT = True
-    _ == _ = False
-
-
+---------------------------------------------------------------------
+-- Prelude.Numeric
+--
+-- Int
 
 data Int = Int
 
+instance Prelude.Eq Int where
+    a == b = catch_any
 
-instance Eq Int where
-    a == b = undefined
-
-
-instance Ord Int where
-    compare a b = undefined
+instance Prelude.Ord Int where
+    compare a b = catch_any
