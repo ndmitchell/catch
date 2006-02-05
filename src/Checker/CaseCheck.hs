19126c642554daa5ba46ff0e3a53b59f10f105bb
ndm <unknown>
2006-02-05 17:20:49 +0000

    Remove commented out dead code

--- src/Checker/CaseCheck.hs
+++ src/Checker/CaseCheck.hs
@@ -17,11 +17,6 @@ import Debug.Trace
 
 traceThis x = x -- trace ("{O{" ++ show x ++ "}C}") x
 
-{-
-
-import Checker.Propagate
-import Checker.Fixpoint
--}
 
 type Output = [String]
 
@@ -80,20 +75,6 @@ solvePending hite p pending x
         
         s@(rs, os) = solves hite (p:pending) x
         rs2 = mapPredLit f rs
-    
-    {-
-        pl = predLit p
-        
-        f rs = case rs of 
-            PredAnd xs -> g $ predAnd $ filter (/= pl) xs
-            PredOr  xs -> g $ predOr  $ filter (/= pl) xs
-        
-        
-        -- g x = trace (show (p,x)) x
-        
-        g x | False = - p `elem` allPredLit x = - error $ show ("solvePending",p, rs)
-            | otherwise = trace ("PENDING: " ++ show p ++ "\nORIGINAL: " ++ show rs ++ "\nFIXED: " ++ show x) (x, os)
-      -}
 
 
 simplifyMid hite x = if simplifyRegular then simplifyReqs False hite x else x
@@ -135,101 +116,6 @@ indent x = "  " ++ x
 indents = map indent
 
 
-{-
-caseCheck2 :: Hite -> IO ()
-caseCheck2 bad_hite = do putStrLn $ disp $ full $ simpler $ generate hite
---                    res <- solveReqs solveProp hite putStrLn (incompleteCases hite)
---                    if res then putStrLn "Success!" else putStrLn "Failed"
-                    
-    where
-        hite = annotate bad_hite
-        
-        
-        disp :: [Reqs] -> String
-        disp x = f 0 x
-            where
-                f 10 (x:xs) = "\nNON TERMINATION"
-                f n  (x:xs) = show n ++ ": " ++ show x ++ "\n" ++ f (n+1) xs
-                f n  []     = "\nMAIN: " ++ show allMains
-
-            
-                allMains = filter isMain $ concatMap allPredLit x
-                
-                isMain (Req (Var a b) _ _) = b == "main"
-                isMain _ = False
-        
-        
-        
-        full :: Reqs -> [Reqs]
-        full reqs | null $ allReq reqs = []
-                  | otherwise = reqs : full (next reqs)
-
-        
-        next :: Reqs -> Reqs
-        next x = simpler $ mapReq (backward hite) $ mapReq (propagate hite) x
-        -}
-        
-{-
-
-solveReqs :: (Hite -> (String -> IO ()) -> Req -> IO Bool) ->
-              Hite -> (String -> IO ()) -> Reqs -> IO Bool
-solveReqs with hite out c = do out $ "Solve: " ++ show c
-                               f c
-    where
-        out2 x = out $ "  " ++ x
-    
-        f PredTrue  = return True
-        f PredFalse = return False
-        f (PredAtom x) = with hite out2 x
-        f (Ors  x) = g or  x
-        f (Ands x) = g and x
-        
-        g comb xs = do res <- mapM f xs
-                       return $ comb res
-
-
-
-solveProp :: Hite -> (String -> IO ()) -> Req -> IO Bool
-solveProp hite out c = do out $ "Start: " ++ show c
-                          let (lefts, rights) = splitEither $ propagate hite c
-                          mapM_ (\x -> out $ "ERROR: propagate: " ++ x) rights
-                          solveReqs solveBack hite out (Ands $ map PredAtom lefts)
-
-
-solveBack :: Hite -> (String -> IO ()) -> Req -> IO Bool
-solveBack hite out c = do out $ "Backwards: " ++ show c
-                          case backward hite c of
-                              Right x -> do out $ "ERROR: backward: " ++ x
-                                            return False
-                              Left (a, b) ->
-                                  do out $ "Result: " ++ show a
-                                     out $ "Calls: " ++ show b
-                                     case fixpoint hite a b of
-                                         Right x -> do out $ "ERROR: fixpoint: " ++ x
-                                                       return False
-                                         Left  x -> do out $ "Fixpoint: " ++ show x
-                                                       solveReqs solveProp hite out x
--}
-{-                         
-
-
-
-solveCase :: Hite -> (String -> IO ()) -> Req -> IO ()
-solveCase hite out c = do out $ "Initial: " ++ show c
-                          case backward hite c of
-                              Right x -> out $ "ERROR: backward: " ++ x
-                              Left (a,b) -> case fixPoint hite a b of
-                                    Right x -> out $ "ERROR: fixPoint: " ++ x
-                                    Left x = 
-                            
-                          let (lefts, rights) = splitEither $ propagate hite c
-    where
-        f x = do out $ "Propagate: " ++ show x
-                 solveCase hite (\a -> out $ "  " ++ a) x
--}
-
-
-
 generate :: Hite -> Reqs
 generate hite = predAnd [predLit $
         Req on pathLambda opts Nothing |
@@ -248,64 +134,3 @@ annotate h = mapFunc f h
         g name (Var x y) = Var x name
         g name x = x
 
-
-{-
-
-
-
-
-checkCase :: Hite -> FuncName -> String
-checkCase hite name = unlines (map ((++) "> " . show) (driver first))
-
-    --error "todo" -- unlines (map show (driver initReqs))
-    --unlines (map showPrettys res ++ [result, answer])
-    where
-        driver x = x : if anyReq x then driver (next x) else []
-    
-        -- the first condition
-        first = simpPredReq (initialPred hite)
-        
-        -- get the next condition from any condition
-        next :: Pred -> Pred
-        next x = simpPredReq $ rootClosure hite $ simpPredReq $ mapReq f x
-            where
-                f req = if reqFunc req == name
-                        then Ands [PredFalse ("@main " ++ show req), res]
-                        else res
-                    where res = propogate hite req
-        
-
-
-
-rootCloses :: Hite -> FuncName -> [Exp]
-rootCloses hite func = filter f (allExpsFunc hite func)
-    where
-        f (Call name args) = callName name == func
-        f _ = False
-
-
-rootClosure :: Hite -> Pred -> Pred
-rootClosure hite prec = mapReq f prec
-    where
-        f req = closure hite (rootCloses hite (reqFunc req)) (PredReq req)
-
-
-
--- bootstrap code
--- get the initial precondition
-
-initialPred :: Hite -> Pred
-initialPred hite = rootClosure hite (incompleteCases hite)
-
-
-incompleteCases :: Hite -> Pred
-incompleteCases hite = Ands $ map (PredReq . f) lst
-    where
-        g (name, expr) = map ((,) name) (allExps expr)
-        allFuncExps = concatMap g (funcs hite)
-        
-        lst = filterSnd (\x -> isCase x && not (isCaseComplete hite x)) allFuncExps
-        
-        f (func, Case (Var var path) opts) = Req func var (asPath path) (fsts opts)
-
--}
