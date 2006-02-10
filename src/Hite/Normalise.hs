64e038f43237dda71a0c9bbe1086ec206961e04c
ndm <unknown>
2006-02-10 13:59:28 +0000

    Make normalise do more, less function tyeps to consider

--- src/Hite/Normalise.hs
+++ src/Hite/Normalise.hs
@@ -1,4 +1,4 @@
-
+    
 module Hite.Normalise(normalise) where
 
 import Hite.Type
@@ -6,6 +6,9 @@ import Hite.Type
 normalise :: Hite -> Hite
 normalise hite = mapExpr f hite
     where
+        f (CallFunc x) = Call (CallFunc x) []
+        f (Call (Call x xs) ys) = Call x (xs++ys)
+        f (Call (CallFunc x) []) = Call (CallFunc x) []
         f (Call x []) = x
         f (Call (Make x []) xs) = Make x xs
         f x = x
