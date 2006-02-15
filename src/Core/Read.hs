f5d129f0040e34539ecb1a29537a992277d1081a
ndm <unknown>
2006-02-15 16:15:41 +0000

    Read in Core lazily

--- src/Core/Read.hs
+++ src/Core/Read.hs
@@ -5,11 +5,24 @@ import Core.Type
 import Core.Reduce
 
 import List
+import Char
 
 readCore :: String -> Core
-readCore ('=':xs) =  readCore (lines xs !! 1)
-readCore xs = reduce $ lambdaFix $ read xs
+readCore ('=':xs) =  readCore $ tail $ dropWhile (/= '\n') xs
+readCore xs = reduce $ lambdaFix $ readSpecial xs
 
+
+-- A special version of Read Core
+-- While only operates on Yhc generated Core
+-- But can be more lazy
+readSpecial :: String -> Core
+readSpecial xs = Core $ concatMap f (lines xs)
+    where
+        f "Core []" = []
+        f "Core" = []
+        f "]" = []
+        f "" = []
+        f x = [read $ tail $ dropWhile isSpace x]
 
 
 -- fix LAMBDA to be Module.LAMBDA
