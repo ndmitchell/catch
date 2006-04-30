e1af07a167b763d4ca78b6c7acf1c75118d0d4e9
Neil Mitchell <ndmitchell@gmail.com>
2006-04-30 22:04:54 +0000

    Some init case checking stuff moved into the case checker proper

--- src/Checker/CaseCheck.hs
+++ src/Checker/CaseCheck.hs
@@ -1,4 +1,38 @@
 
 module Checker.CaseCheck(caseCheck) where
 
-import Checker.Solver
+import qualified Checker.Solver
+
+import IO
+import Hite
+import Constraint
+import General.General
+
+
+caseCheck :: Handle -> Hite -> IO ()
+caseCheck hndl hite = --error $ show $ initErrors $ removeError hite
+
+     Checker.Solver.caseCheck hndl hite
+
+
+
+removeError :: Hite -> Hite
+removeError hite = hite{funcs = filter ((/=) "error" . funcName) (funcs hite)}
+
+
+initErrors :: Hite -> [(String, Reqs)]
+initErrors hite = [(head errors, condToPred cond) |
+        func <- funcs hite, let MCase alts = body func,
+        MCaseAlt cond val <- alts, let errors = getErrors val, not (null errors)]
+    where
+        getErrors val = [getMsg args | Call (CallFunc "error") args <- allExpr val]
+        condToPred cond = predAnd $ map (predLit . f) cond
+        
+        getMsg [Msg x] = x
+        getMsg [x] = output x
+        getMsg xs = show xs
+        
+        f (expr, cond) = Req expr pathLambda [cond]
+    
+    
+    
