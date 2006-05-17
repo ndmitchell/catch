9830a4dcc8d99131483dcbb0b1747897f3a39e0c
Neil Mitchell <ndmitchell@gmail.com>
2006-05-17 10:41:17 +0000

    Write custom instances for Eq Req and Eq ReqAll, which don't compare the whole Hite data structure

--- src/Reqs/Type.hs
+++ src/Reqs/Type.hs
@@ -20,10 +20,15 @@ import List
 
 
 data Req = Req {reqExpr :: Expr, reqPath :: Path String, reqCtors :: [CtorName], reqHite :: Hite}
-           deriving Eq
            
 data ReqAll = ReqAll {reqForall :: FuncName, reqWithin :: Reqs}
-              deriving Eq
+
+
+instance Eq Req where
+    (Req a1 b1 c1 _) == (Req a2 b2 c2 _) = a1 == a2 && b1 == b2 && (c1 `setEq` c2)
+    
+instance Eq ReqAll where
+    (ReqAll a1 b1) == (ReqAll a2 b2) = a1 == a2 && b1 == b2
 
 
 type Reqs = Pred Req
