f013592d7332f0c17c88dd8ddad74c7310e27e8f
Neil Mitchell <ndmitchell@gmail.com>
2006-05-30 10:47:29 +0000

    Change the drawing code to have a new representation

--- src/Graph/Draw.hs
+++ src/Graph/Draw.hs
@@ -15,25 +15,26 @@ drawGraph graph file = do writeFile (file ++ ".dot") (showGraph graph)
 showGraph :: Graph -> String
 showGraph xs = unlines $ ["Digraph catch{"] ++ concat (zipWith f [0..] xs) ++ ["}"]
     where
-        nod :: Int -> String
-        nod n = "n" ++ show n
-    
-        f n (Node name [] [GraphEnd]) = [nod n ++ "[label=\"End\",color=\"red\"]"]
-        f n (Node name edges rewrite) = (nn ++ "[label=\"" ++ show n ++ "," ++ name ++ "\"]")
-                                       : map (g nn) edges
-                                       ++ showRewrite nn rewrite
-            where nn = nod n
-        
-        g from to = from ++ "->" ++ nod to ++ "[label=\"\"]"
+        f n (Node name edges rewrite) =
+              ["n" ++ show n ++ "0[label=\"" ++ show n ++ "," ++ name ++ "\",color=\"green\"]"] ++
+              showRewrite ("n" ++ show n) 0 rewrite edges
+              
 
-showRewrite :: String -> [Rewrite] -> [String]
-showRewrite node [] = []
-showRewrite node [Rewrite a b] = 
-    [node ++ "->" ++ node ++ "e[label=\"E\"]"
-    ,node ++ "->" ++ node ++ "b[label=\"B\"]"
-    ]
-    ++ showGExp (node ++ "b") a
-    ++ showGExp (node ++"e") b
+showRewrite :: String -> Int -> [Rewrite] -> [Int] -> [String]
+showRewrite node i [] edges = [node ++ show i ++ "->n" ++ show e ++ "0" | e <- edges]
+showRewrite node i (Rewrite a b:xs) edges = 
+    [node ++ show i ++ "->" ++ node ++ show (i+1)
+    ,node ++ show (i+1) ++ "->" ++ node ++ show (i+2) ++ "[color=\"blue\"]"
+    ] ++
+    showGExp (node ++ show (i+1)) a ++
+    showGExp (node ++ show (i+2)) b ++
+    showRewrite node (i+2) xs edges
+
+showRewrite node i (GraphEnd:xs) edges = 
+    [node ++ show i ++ "->" ++ node ++ show (i+1)
+    ,node ++ show (i+1) ++ "[label=\"End\",color=\"red\"]"
+    ] ++
+    showRewrite node (i+1) xs edges
 
 
 
