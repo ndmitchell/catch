d8abfd8d8c0dabe87f504ee63600fddf2f47628e
Neil Mitchell <ndmitchell@gmail.com>
2006-06-13 16:10:58 +0000

    Add pretty printing to HTML for typey values, initial try

--- src/Typey/Pretty.hs
+++ src/Typey/Pretty.hs
@@ -11,17 +11,45 @@ typeyPretty file items = writeFile (file ++ ".htm") res
         res = prefix ++ concatMap showItem items ++ suffix
 
 
-prefix = "<html><body>"
+prefix = "<html><head><style type='text/css'>\n" ++ unlines style ++ "\n</style></head><body>"
 suffix = "</body></html>"
 
+style =
+    ["table {border:1px solid black;}"
+    ,"td {border:1px solid red;}"
+    ,".tb,.t,.b {border:1px solid blue;}"
+    ,".t {border-bottom:0;}"
+    ,".b {border-top:0;}"
+    ,".o {font-size: 2em;}"
+    ]
 
 showItem :: (FuncName, [Subtype], Subtype) -> String
 showItem (name, args, res) =
     "<table><tr>" ++
     "<td>" ++ name ++ " ::</td>" ++
-    concat ["<td>" ++ showSubtype a ++ " -&gt;</td>" | a <- args] ++
-    "<td>" ++ showSubtype res ++ "</td>"
+    concat ["<td>" ++ nice a ++ "</td><td>-&gt;</td>" | a <- args] ++
+    "<td>" ++ nice res ++ "</td>" ++
+    "</tr></table>\n"
 
 
-showSubtype :: Subtype -> String
-showSubtype x = show x
+class Nice a where
+    nice :: a -> String
+    nices :: [a] -> String
+    
+    nices = concatMap nice
+
+
+instance Nice Subtype where
+    nice (Atom x) = show (Atom x)
+    nice (Subtype (a :@ b) ([] :@ d)) = 
+        "<table><tr><td class='tb'>" ++ nices a ++ "</td><td class='o'>(</td><td>" ++
+        nices b ++ "</td><td class='o'>)</td></tr></table>"
+    nice (Subtype (a :@ b) (c :@ d)) =
+        "<table><tr><td class='t'>" ++ nices a ++ "</td><td rowspan='2'>(</td>" ++
+        "<td>" ++ nices b ++ "</td><td rowspan='2'>)</td></tr>" ++
+        "<tr><td class='b'>" ++ nices c ++ "</td><td>" ++ nices d ++ "</td></tr></table>"
+
+instance Nice Subvalue where
+    nices x = show x
+    nice x = show x
+
