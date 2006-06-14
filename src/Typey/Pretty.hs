9fb9771d3cdb2deb1877bca7f01e90a196d9b3ee
Neil Mitchell <ndmitchell@gmail.com>
2006-06-14 14:21:03 +0000

    Add improvements to the HTML output

--- src/Typey/Pretty.hs
+++ src/Typey/Pretty.hs
@@ -3,6 +3,7 @@ module Typey.Pretty(typeyPretty) where
 
 import Hite
 import Typey.Type
+import Data.List
 
 
 typeyPretty :: FilePath -> [(FuncName, [Subtype], Subtype)] -> IO ()
@@ -15,19 +16,18 @@ prefix = "<html><head><style type='text/css'>\n" ++ unlines style ++ "\n</style>
 suffix = "</body></html>"
 
 style =
-    ["table {border:1px solid black;}"
-    ,"td {border:1px solid red;}"
-    ,".tb,.t,.b {border:1px solid blue;}"
+    [".tb,.t,.b {background-color: #ddd; text-align:center;}"
     ,".t {border-bottom:0;}"
     ,".b {border-top:0;}"
-    ,".o {font-size: 2em;}"
+    ,".o1 {font-size: 100%;}"
+    ,".o2 {font-size: 200%;}"
     ]
 
 showItem :: (FuncName, [Subtype], Subtype) -> String
 showItem (name, args, res) =
     "<table><tr>" ++
     "<td>" ++ name ++ " ::</td>" ++
-    concat ["<td>" ++ nice a ++ "</td><td>-&gt;</td>" | a <- args] ++
+    concat ["<td>" ++ nice a ++ "</td><td>&rarr;</td>" | a <- args] ++
     "<td>" ++ nice res ++ "</td>" ++
     "</tr></table>\n"
 
@@ -40,16 +40,22 @@ class Nice a where
 
 
 instance Nice Subtype where
-    nice (Atom x) = show (Atom x)
+    nice (Atom x) = nices x
     nice (Subtype (a :@ b) ([] :@ d)) = 
-        "<table><tr><td class='tb'>" ++ nices a ++ "</td><td class='o'>(</td><td>" ++
-        nices b ++ "</td><td class='o'>)</td></tr></table>"
-    nice (Subtype (a :@ b) (c :@ d)) =
-        "<table><tr><td class='t'>" ++ nices a ++ "</td><td rowspan='2'>(</td>" ++
-        "<td>" ++ nices b ++ "</td><td rowspan='2'>)</td></tr>" ++
+        "<table><tr><td class='tb'>" ++ nices a ++ "</td><td class='o1'>(</td><td>" ++
+        nices b ++ "</td><td class='o1'>)</td></tr></table>"
+    nice s@(Subtype (a :@ b) (c :@ d)) =
+        "<table cellspacing='0'><tr><td class='t'>" ++ nices a ++
+        "</td><td rowspan='2' class='o2'>(</td>" ++
+        "<td>" ++ nices b ++ "</td><td rowspan='2' class='o2'>)</td></tr>" ++
         "<tr><td class='b'>" ++ nices c ++ "</td><td>" ++ nices d ++ "</td></tr></table>"
 
-instance Nice Subvalue where
-    nices x = show x
-    nice x = show x
 
+instance Nice Subvalue where
+    nices [] = "?"
+    nices [x] = nice x
+    nices xs = "[" ++ (concat $ intersperse "," $ map nice xs) ++ "]"
+
+    nice Bot = "&perp;"
+    nice (SVar n) = "#" ++ show n
+    nice (SCtor s) = s
