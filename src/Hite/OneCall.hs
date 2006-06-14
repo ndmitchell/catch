f36dd3047a361721320c7f4ae5b609c876ef9073
Neil Mitchell <ndmitchell@gmail.com>
2006-06-14 16:17:32 +0000

    Implement OneCall, and realise its semantics are actually two calls

--- src/Hite/OneCall.hs
+++ src/Hite/OneCall.hs
@@ -1,5 +1,6 @@
 
--- Make sure there are no nested calls
+-- Make sure there are no deeply nested calls
+-- 2 level depth is required
 -- Easy to do by introducing extra functions
 
 module Hite.OneCall(cmd) where
@@ -12,6 +13,29 @@ cmd = cmdHitePure (const oneCall) "onecall"
 
 
 oneCall :: Hite -> Hite
-oneCall x = x
+oneCall hite = hite{funcs = concatMap makeOne (funcs hite)}
+    where
+    
+makeOne :: Func -> [Func]
+makeOne (Func name args body extra) = Func name args (MCase $ map fst res) extra : concatMap snd res
+    where
+        res = [g o | let MCase opts = body, o <- opts]
+
+        g :: MCaseAlt -> (MCaseAlt, [Func])
+        g (MCaseAlt p x) | callDepth x > 2 = (MCaseAlt p x, [])
+                         | otherwise = (MCaseAlt p callon, makeOne (Func newname newargs newbody extra))
+            where
+                callon = Call (CallFunc newname) (map fst shallow ++ map Var args)
+                newbody = MCase [MCaseAlt p (mapExpr rep x)]
+                newname = name ++ "_one"
+                newargs = map snd shallow ++ args
+                shallow = zip [c | c@(Call _ _) <- allExpr x, callDepth c == 1] ["_one_" ++ show x | x <- [0..]]
+                
+                rep x = case lookup x shallow of
+                            Nothing -> x
+                            Just y -> Var y
 
 
+
+callDepth :: Expr -> Int
+callDepth x = maximum (0 : [(maximum $ map callDepth xs) + 1 | Call _ xs <- allExpr x])
