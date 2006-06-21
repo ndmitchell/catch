483495fe79252e691baf7af3ba5f3fa4c9b0f8f5
Neil Mitchell <ndmitchell@gmail.com>
2006-06-21 14:49:06 +0000

    Add a transitive example, from Matt

---
+++ src/Example/Transitive.hs
@@ -0,0 +1,17 @@
+-- From Matt :)
+
+module Transitive where
+
+main a b c = assert (trans a b c)
+
+trans a b c = imp ((a `eq` b) && (b `eq` c)) (a `eq` c)
+
+assert True x = x
+
+eq True True = True
+eq False False = True
+eq _ _ = False
+
+imp True True = True
+imp False _ = True
+imp _ _ = False
