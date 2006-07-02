8a9195dc2d46c005003e03ad9e9165f272d3febb
Neil Mitchell <ndmitchell@gmail.com>
2006-07-02 16:54:06 +0000

    Move some code into Subtype.hs

--- src/Typey/CtorTypes.hs
+++ src/Typey/CtorTypes.hs
@@ -35,18 +35,3 @@ smallToLarge n ctr xs = map f xs
     where
         f Self = Bind2T (Ctor2T ctr) [Free2T [numToChr i] | i <- [0..n-1]]
         f (FreeS i) = Free2T [numToChr i]
-
-
-{-
-typePermutations :: DataT SmallT -> [TSubtype]
-typePermutations (DataT n xs) =
-        [TBind [x]| x <- nper] ++ [TBind [x,y] | x <- rper, y <- rper++nper]
-    where
-        (rper,nper) = (map f rctr, map f nctr)
-        (rctr,nctr) = partition isRecursive xs
-
-        f (CtorT name xs) = TPair [name] $ map g [0..n-1]
-            where
-                g i = if i `elem` frees then TFree [[numToChr i]] else TFree []
-                frees = nub [i | FreeS i <- xs]
--}
