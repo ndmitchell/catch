5ba02661434b8e35ca0e59fc6a2fcdc4b4be39f7
Neil Mitchell <ndmitchell@gmail.com>
2006-07-03 19:46:35 +0000

    Move stuff into Typey.Unify

--- src/Typey/Eliminate.hs
+++ src/Typey/Eliminate.hs
@@ -3,6 +3,7 @@ module Typey.Eliminate(eliminate) where
 
 import Typey.Type
 import Typey.Subtype
+import Typey.Unify
 import Hite
 import General.General
 import Data.Maybe
@@ -52,22 +53,6 @@ newTypeList logger env@(hite,datam,datat,funct) =
             logger $ if same then " KEEP\n" else " ===> " ++ show res3 ++ "\n"
             return (not same, TArr args res3)
 
-
-{-
--- given a datat and a funct, check that the function can have this type
-validType :: Logger -> Env -> String -> TSubtype -> IO Bool
-validType logger env@(hite,datam,datat,funct) funcname (TArr argt res) =
-        -- logger (" $ " ++ show (resb,resn) ++ " $") >>
-        (
-        if isTBot res then return $ not $ null resb
-        else if null resn then return False
-        else return $ res `isTSubset` unionList resn
-        )
-    where
-        (resb,resn) = partition isTBot ress
-
-validType logger env funcname res = validType logger env funcname (TArr [] res)
--}
 
 getFuncType :: Env -> FuncName -> [TSubtype] -> TSubtype
 getFuncType env@(hite,datam,datat,funct) funcname argt = unionList ress
@@ -131,52 +116,6 @@ getType env@(hite,datam,datat,funct) args expr = traceNone ("getType " ++ show a
             where
                 f (TArr a1 _) (TArr a2 _) = a1 == a2
                 g xs@((TArr a r):_) = TArr a $ unionList [x | TArr _ x <- xs]
-
-
-applyUnify :: [(String, TSubtype)] -> TSubtype -> TSubtype
-applyUnify dat (TBind xs) = TBind $ map f xs
-    where
-        f (TPair a1 b1) = TPair a1 (map (applyUnify dat) b1)
-
-applyUnify dat (TFree []) = TFree []
-applyUnify dat o@(TFree [n]) = case lookup n dat of
-                                          Nothing -> o
-                                          Just x -> x
-applyUnify dat (TFree ns) = unionList $ map (applyUnify dat . TFree . box) ns
-applyUnify dat (TFunc x) = tFunc $ map f x
-    where f (TArr a b) = TArr (map (applyUnify dat) a) (applyUnify dat b)
-applyUnify dat TBot = TBot
-applyUnify dat x = error $ show ("applyUnify",dat,x)
-
-
-
--- roughly x `isTSubset` y (for constructors at least)
--- figure what a variable in x would have to be mapped to
-unify :: TSubtype -> TSubtype -> Maybe [(String, TSubtype)]
-unify (TBind (x:xs)) (TBind (y:ys)) = liftM concat $ sequence $ f x y : zipWith g xs ys
-    where
-        f (TPair a1 b1) (TPair a2 b2) =
-            if null $ a1 \\ a2
-            then liftM concat $ sequence $ zipWith unify b1 b2
-            else Nothing
-
-        -- demand equality for child types, because of the powerset rule
-        -- kinda hacky, needs formalising
-        g p1@(TPair a1 b1) p2@(TPair a2 b2) =
-            if a1 `setEq` a2 then f p1 p2 else Nothing
-            
-unify (TFree []) _ = Just []
-unify (TFree []) TBot = Nothing
-unify TBot (TFree []) = Nothing
-unify TBot TBot = Just []
-unify _ (TFree []) = Just []
-unify (TFree [a]) x = Just [(a,x)]
-unify TBot (TFree [a]) = Nothing
---unify (TArr a1 b1) (TArr a2 b2) =
---    liftM concat $ sequence $ zipWithEq unify (b1:a1) (b2:a2)
-
-
-unify x y = error $ show ("unify",x,y)
 
 
 
