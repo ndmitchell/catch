27af9c77c6ac8d9a63ddf412d5873eab7604c9ed
Neil Mitchell <ndmitchell@gmail.com>
2006-07-15 12:26:36 +0000

    Add better pretty printing for reasons in the HTML

--- src/Typey/Reason.hs
+++ src/Typey/Reason.hs
@@ -8,6 +8,7 @@ module Typey.Reason(
 import Hite
 import IO
 import Typey.Subtype
+import General.General
 
 
 data Reason = ReasonArgs [(FuncArg, TSubtype)] Reason
@@ -26,13 +27,35 @@ reasonSubtype (ReasonFollow x _ _) = x
 reasonSubtype (ReasonApply x _ _) = x
 
 
+initial = unlines 
+    ["<html>"
+    ,"<head>"
+    ,"<style type='text/css'>"
+    ,"td {text-align: center;}"
+    ,"tr {vertical-align:bottom;}"
+    ,".overline {border-top:1px solid black;}"
+    ,"</style>"
+    ,"</head>"
+    ,"<body>"
+    ]
+
+final = unlines
+    ["</body>"
+    ,"</html>"
+    ]
+
+
+
 type ReasonH = Handle
 
 reasonInit :: FilePath -> IO ReasonH
-reasonInit s = openFile s WriteMode
+reasonInit s = do r <- openFile s WriteMode
+                  hPutStrLn r initial
+                  return r
 
 reasonTerm :: ReasonH -> IO ()
-reasonTerm r = hClose r
+reasonTerm r = do hPutStrLn r final
+                  hClose r
 
 reasonSection :: ReasonH -> String -> IO ()
 reasonSection r = reasonDo r 1
@@ -49,4 +72,29 @@ reasonDo r n s = hPutStrLn r $ "<" ++ tag ++ s ++ "</" ++ tag
 
    
 reasonShow :: ReasonH -> Reason -> IO ()
-reasonShow r reason = hPutStrLn r (show reason)
+reasonShow r reason = hPutStrLn r (htmlReason reason)
+
+
+
+
+htmlReason :: Reason -> String
+htmlReason (ReasonArgs xs r) = concat [a ++ " := " ++ htmlSubtype b ++ "<br/>\n" | (a,b) <- xs] ++ htmlReason r
+htmlReason (ReasonUnion r [x]) = htmlReason x
+
+htmlReason (ReasonLookup t x) =
+    "<table><tr><td>" ++ htmlSubtype t ++ "</td></tr>" ++
+    "<tr><td class='overline'>" ++ x ++ "</td></tr></table>"
+
+htmlReason (ReasonApply res lhs rhs) =
+    "<table><tr><td>" ++ htmlReason lhs ++ "</td><td>" ++ htmlReason rhs ++ "</td></tr>" ++
+    "<tr><td colspan='2' class='overline'>" ++ htmlSubtype res ++ "</td></tr></table>"
+
+htmlReason (ReasonFollow res x y) =
+    "<table><tr><td>" ++ htmlReason y ++ "</td><td>." ++ x ++ "</td></tr>" ++
+    "<tr><td colspan='2' class='overline'>" ++ htmlSubtype res ++ "</td></tr></table>"
+
+htmlReason x = show x
+
+
+htmlSubtype :: TSubtype -> String
+htmlSubtype x = output x
