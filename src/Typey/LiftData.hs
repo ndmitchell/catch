5b9fd2e1e9b13b813cace2e77f35683f3b792203
Neil Mitchell <ndmitchell@gmail.com>
2006-07-18 13:33:08 +0000

    Hack in the data types needed to LiftData

--- src/Typey/LiftData.hs
+++ src/Typey/LiftData.hs
@@ -6,7 +6,13 @@ import Typey.Type
 
 injectData = [("Bool",DataT 0 [CtorT "True" [], CtorT "False" []])
              ,("[]",DataT 1 [CtorT "[]" [], CtorT ":" [FreeS 0, Self]])
-             ]
+             ,("Compare",DataT 0 $ map (`CtorT` []) ["EQ","LT","GT"])
+             ] ++
+             map makeTuple [1..8]
+    where
+        makeTuple n = (tup, DataT n [CtorT tup [FreeS (i-1) | i <- [1..n]]])
+            where
+                tup = "Tup" ++ show n
 
 liftData :: DataM LargeT -> FuncM -> (DataM SmallT, FuncM)
 liftData datas funcs = (injectData, funcs)
