ec726745234fa1d35030f5688342809414b7f804
Neil Mitchell <ndmitchell@gmail.com>
2006-07-22 22:34:59 +0000

    Change the type of an Abstract List to retain more structure

--- src/Typey/Abstract.hs
+++ src/Typey/Abstract.hs
@@ -11,13 +11,11 @@ import Debug.Trace
 
 
 data Abstract = Bit Bool
-              | List [Abstract]
+              | List Bool [Abstract] [Abstract]
               | AbsBottom
               | AbsVoid
               deriving Eq
 
-b0 = Bit False
-b1 = Bit True
 
 fromBit (Bit x) = x
 
@@ -25,27 +23,29 @@ instance Show Abstract where
     show (Bit x) = if x then "1" else "0"
     show (AbsBottom) = "!"
     show (AbsVoid) = "#"
-    show (List (Bit True:xs)) = "[!" ++ concatMap show xs ++ "]"
-    show (List xs) = "[" ++ concatMap show xs ++ "]"
+    show (List b xs ys) = "[" ++ bot ++ concatMap show (xs++ys) ++ "]"
+        where bot = if b then "!" else "_"
 
 
 absBottom :: Abstract -> Bool
 absBottom (AbsBottom) = True
-absBottom (List (Bit b:xs)) = b || any absBottom xs
-absBottom (List xs) = any absBottom xs
+absBottom (List b xs ys) = b || any absBottom (xs++ys)
 absBottom _ = False
 
 
-
 unionAbs :: [Abstract] -> Abstract
-unionAbs xs = foldr f AbsVoid xs
+unionAbs = unionAbsNote ""
+
+unionAbsNote :: String -> [Abstract] -> Abstract
+unionAbsNote msg xs = foldr f AbsVoid xs
     where
         f AbsVoid x = x
         f x AbsVoid = x
-        f AbsBottom (List (x:xs)) = List (b1:xs)
-        f (List (x:xs)) AbsBottom = List (b1:xs)
+        f AbsBottom (List b xs ys) = List True xs ys
+        f (List b xs ys) AbsBottom = List True xs ys
         f (Bit i) (Bit j) = Bit (i || j)
-        f (List i) (List j) = List $ zipWithEq f i j
+        f (List b1 xs1 ys1) (List b2 xs2 ys2) = List (b1||b2) (zipWithEq f xs1 xs2) (zipWithEq f ys1 ys2)
+        f a b = error $ "unionAbs (" ++ msg ++ ") failed on " ++ show xs ++ " with " ++ show (a,b)
 
 
 
@@ -55,28 +55,29 @@ unionAbs xs = foldr f AbsVoid xs
 getAbstract :: DataM SmallT -> Large2T -> Abstract
 getAbstract datam x = f x
     where
-        f (Free2T _) = List [b0]
+        f (Free2T _) = List False [] []
         f (Arr2T _ _) = error "getAbstract: Cannot abstract for a higher order function"
         f (Ctor2T ctor) = f $ Bind2T (Ctor2T ctor) []
         
-        f (Bind2T (Ctor2T ctor) args) = List $ b0 : lst ++ rlst
+        f (Bind2T (Ctor2T ctor) args) = List False lst rlst
             where
                 rlst = if any isRecursive ctrs then lst else []
-                lst = replicate (length ctrs) b1 ++ map f args
+                lst = replicate (length ctrs) (Bit True) ++ map f args
                 (DataT n ctrs) = lookupJust ctor datam
 
 
 hasCtorAbs :: DataM SmallT -> Abstract -> CtorName -> Bool
-hasCtorAbs datam (List x) name = fromBit (x !! i)
+hasCtorAbs datam (List b xs ys) name = fromBit (xs !! i)
     where
         i = head [i | (_, DataT n ctrs) <- datam,
-                      (i, CtorT n _) <- zip [1..] ctrs, n == name]
+                      (i, CtorT n _) <- zip [0..] ctrs, n == name]
 
 
 followSelAbs :: Hite -> DataM SmallT -> Abstract -> String -> Abstract
-followSelAbs hite datam (List (x:xs)) name = assert (length (x:xs) == len) $
+followSelAbs hite datam (List b xs ys) name = assertNote "followSelAbs"
+        (len - 1 == length (xs++ys) && length xs == lenHalf) $
         case as !! i of
-            Self -> List $ x : take lenHalf xs ++ take lenHalf xs
+            Self -> List b ys ys
             FreeS i -> xs !! (i + length ctrs)
     where
         lenHalf = length ctrs + n
@@ -91,36 +92,28 @@ followSelAbs hite datam (List (x:xs)) name = assert (length (x:xs) == len) $
 
 
 makeAbs :: DataM SmallT -> CtorName -> [Abstract] -> Abstract
-makeAbs datam name args = assert (length as == length args && length void == len) $
-        res
+makeAbs datam name args = assert (length as == length args) $
+        foldr f (addBase void) (zip as args)
     where
-        res = List (foldr f (addBase void) (zip as args))
-        addBase x = x !!! (i+1, b1)
+        addBase (List b xs ys) = List b (xs !!! (i, Bit True)) ys
         
-        f :: (SmallT, Abstract) -> [Abstract] -> [Abstract]
-        f (FreeS i, b) x = x !!! (ii, unionAbs [b, x !! ii])
-            where ii = i + length ctrs + 1
-    
+        f :: (SmallT, Abstract) -> Abstract -> Abstract
+        f (FreeS i, b) (List x x1 x2) = List x (x1 !!! (ii, unionAbs [b, x1 !! ii])) x2
+            where ii = i + length ctrs
+        
         f (Self, AbsVoid) x = x
-        f (Self, List (b:bs)) (x:xs) = unionAbs [x,b] : xs1 ++ bss
-            where
-                (xs1,xs2) = splitAt lenHalf xs
-                (bs1,bs2) = splitAt lenHalf bs
-                List bss = unionAbs [List bs1, List bs2, List xs2]
-    
+        f (Self, List b bs1 bs2) (List bx xs1 xs2) = List (b||bx) xs1 bss
+            where bss = map unionAbs $ transpose [bs1, bs2, xs2]
+
         f a b = error $ "makeAbs.f " ++ show (a,b)
     
-        void = b0 : concat (replicate (if rec then 2 else 1) voidHalf)
-        voidHalf = replicate (length ctrs) b0 ++ replicate n AbsVoid
+        void = List False voidHalf (if rec then voidHalf else [])
+        voidHalf = replicate (length ctrs) (Bit False) ++ replicate n AbsVoid
         
         lenHalf = length ctrs + n
         len = 1 + (lenHalf * (if rec then 2 else 1))
+        
         rec = any isRecursive ctrs
         (DataT n ctrs, CtorT _ as, i) = head [(d,c,i) | (_,d@(DataT n cs)) <- datam,
                                                    (i, c@(CtorT n2 _)) <- zip [0..] cs,
                                                    n2 == name]
-
-unionAbsTrace msg x =
-        trace (show msg ++ ": unionAbs " ++ show x) $
-        trace (show res) res
-    where res = unionAbs x
