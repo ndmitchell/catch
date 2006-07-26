765c0623a74947fa77fab5a27e47d9b58f0f8311
Neil Mitchell <ndmitchell@gmail.com>
2006-07-26 01:06:54 +0000

    Fix LiftData to actually generate the data, rather than inject it

--- src/Typey/LiftData.hs
+++ src/Typey/LiftData.hs
@@ -3,21 +3,21 @@ module Typey.LiftData(liftData, getDatas) where
 
 import Hite
 import Typey.Type
+import Data.List
+import Data.Maybe
 
-
-injectData = [("Bool",DataT 0 [CtorT "True" [], CtorT "False" []])
-             ,("[]",DataT 1 [CtorT "[]" [], CtorT ":" [FreeS 0, Self]])
-             ,("Compare",DataT 0 $ map (`CtorT` []) ["EQ","LT","GT"])
-             ] ++
-             map makeTuple [0..8]
-    where
-        makeTuple n = (tup, DataT n [CtorT tup [FreeS (i-1) | i <- [1..n]]])
-            where
-                tup = "Tup" ++ show n
 
 liftData :: Hite -> FuncM -> (DataM SmallT, FuncM)
 liftData hite funcs = (getDatas hite, funcs)
 
 
 getDatas :: Hite -> DataM SmallT
-getDatas x = injectData
+getDatas hite = map f (datas hite)
+    where
+        f (Data dname ctrs free) = (dname, DataT (length free) (map g ctrs))
+            where
+                g (Ctor cname _ tys) = CtorT cname (map h tys)
+        
+                h (TyFree n) = FreeS $ fromJust $ elemIndex n free 
+                h (TyCon x xs) | x == dname && map TyFree free == xs = Self
+                h x = error $ "getDatas, " ++ dname ++ ", can't make small " ++ show x
