bd06d8603949e233dca43cbfc53431a2cc81408e
Neil Mitchell <ndmitchell@gmail.com>
2006-08-24 23:25:27 +0000

    Make the output nicer

--- src/Train/Backward.hs
+++ src/Train/Backward.hs
@@ -4,11 +4,12 @@ module Train.Backward(backward) where
 import Train.Type
 import Train.Template
 import System.IO
+import General.General
 
 
 backward :: ZHite -> Template -> Handle -> Scopes -> IO Scopes
 backward zhite template hndl x = do
-	hPutStrLn hndl ("Solving: " ++ show x)
+	hPutStrLn hndl ("Solving: " ++ output x)
 	return x
 
 
--- src/Train/Type.hs
+++ src/Train/Type.hs
@@ -4,6 +4,7 @@ module Train.Type where
 import Data.Predicate
 import Data.List
 import Hite
+import General.General
 
 
 data ZHite = ZHite [Data] [ZFunc]
@@ -39,3 +40,23 @@ instance PredLit Scope
 
 instance PredLitNot Req where
 	litNot (Req hite expr path ctors) = predLit $ Req hite expr path (getCtorsFromCtor hite (head ctors) \\ ctors)
+
+
+instance Output ZExpr where
+	output x = output $ f x
+		where
+			f (ZCall x xs) = Call (CallFunc x) (map f xs)
+			f (ZMake x xs) = Make x (map f xs)
+			f (ZSel x y) = Sel (f x) y
+			f (ZVar x) = Var x
+
+instance Output Scope where
+	output (Scope name reqs) = "(\\forall " ++ name ++ ", " ++ output reqs ++ ")"
+
+instance Output Req where
+	output (Req _ expr path ctor) =
+		output expr ++ concatMap (('.':) . output) path ++ strSet ctor
+
+instance Output Path where
+	output (PathAtom x) = x
+	output (PathStar x) = x ++ "*"
