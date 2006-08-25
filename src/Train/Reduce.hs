84342ddf62721d288310658c388b55b3b8fadb2e
Neil Mitchell <ndmitchell@gmail.com>
2006-08-25 02:37:19 +0000

    Add a very primative case for ZMake, needs fixing properly

--- src/Train/Reduce.hs
+++ src/Train/Reduce.hs
@@ -11,4 +11,5 @@ reduce req@(Req hite expr path ctors) = case expr of
 	ZCall{} -> predLit $ req
 	ZVar{} -> predLit $ req
 	ZSel x y -> reduce (Req hite x (path `integrate` y) ctors)
+	ZMake y xs | null path -> predBool $ y `elem` ctors
 	_ -> error $ "reduce: " ++ output req
