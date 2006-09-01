cb7811eb8e41bfbd07c3e8a570a8cebe708b596f
Neil Mitchell <ndmitchell@gmail.com>
2006-09-01 22:34:06 +0000

    Implement decoding of the transform stuff

--- src/Transform/All.hs
+++ src/Transform/All.hs
@@ -4,9 +4,10 @@ module Transform.All(transform) where
 import Hite.Type
 import Transform.Type
 import Transform.Encode
+import Transform.Decode
 import Transform.Driver
 import General.General
 
 
 transform :: Hite -> Hite
-transform x = error $ output $ applyTransform $ encode x
+transform x = decode $ applyTransform $ encode x
---
+++ src/Transform/Decode.hs
@@ -0,0 +1,32 @@
+
+module Transform.Decode(decode) where
+
+import Hite.Type as R
+import Transform.Type as L
+import General.General
+
+decode :: IHite -> Hite
+decode (IHite datas funcs) = Hite datas (map deFunc funcs)
+
+
+deFunc :: IFunc -> Func
+deFunc (L.Func name args body _) = R.Func name (map deVar args) (deExpr body) ""
+
+
+deExpr :: IExpr -> Expr
+deExpr x = case x of
+		L.Make x xs -> R.Make x (fs xs)
+		L.Var x -> R.Var $ deVar x
+		L.Sel x y -> R.Sel (f x) y
+		L.Case x y -> R.Case (f x) [(a,f b) | (a,b) <- y]
+		L.Call x xs -> R.Call (R.CallFunc x) (fs xs)
+		L.Error x -> R.Error x
+		L.Unknown -> R.Unknown
+		_ -> error $ "Transform.Decode.deExpr: " ++ show x
+	where
+		f = deExpr
+		fs = map f
+
+
+deVar :: Int -> String
+deVar x = "v" ++ show x
