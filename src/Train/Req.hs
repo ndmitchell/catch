a8d900a92ed5bbf1c11b249f719a499c85b0e361
Neil Mitchell <ndmitchell@gmail.com>
2006-09-02 20:03:36 +0000

    Move the Req stuff from Train.Type to Train.Req

---
+++ src/Train/Req.hs
@@ -0,0 +1,112 @@
+
+module Train.Req where
+
+import Hite
+import General.General
+import Train.Path
+import Data.BDD
+import Data.List
+
+
+-- module Train.Type
+-- here to break cyclic dependancies
+
+data ZExpr = ZCall FuncName [ZExpr]
+		   | ZMake CtorName [ZExpr]
+		   | ZSel ZExpr CtorArg
+		   | ZVar FuncArg
+		   | ZAny
+		   deriving (Eq, Show, Ord)
+
+
+instance Output ZExpr where
+	output x = case x of
+		ZCall x [] -> x
+		ZCall x xs -> "(" ++ x ++ concatMap ((' ':) . output) xs ++ ")"
+		ZMake x xs -> output (ZCall x xs)
+		ZSel x y -> output x ++ "." ++ y
+		ZVar x -> x
+		ZAny -> "_"
+
+-- end
+
+
+
+type Scopes = BDD Scope
+data Scope = Scope FuncName Reqs
+			 deriving (Eq, Ord, Show)
+
+
+type Reqs = BDD Req
+data Req = Req Hite ZExpr Path [CtorName]
+		   deriving (Show)
+
+instance Eq Req where
+	(Req _ a1 b1 c1) == (Req _ a2 b2 c2) = a1 == a2 && b1 == b2 && c1 == c2
+
+instance Ord Req where
+	compare (Req _ a1 b1 c1) (Req _ a2 b2 c2) = compare (a1,b1,c1) (a2,b2,c2)
+
+
+newReq :: Hite -> ZExpr -> Path -> [CtorName] -> Req
+newReq hite zexpr path ctors
+	| path == newPath hite ["tl"] && ctors == ["[]"] = Req hite zexpr (emptyPath hite) ctors
+	| otherwise = Req hite zexpr path (nub $ sort ctors)
+
+newReqs :: Hite -> ZExpr -> Path -> [CtorName] -> Reqs
+newReqs hite zexpr path ctors | null ctors = bddFalse
+							  | ctors `setEq` baseSet = bddTrue
+							  | otherwise = bddLit $ newReq hite zexpr path ctors
+	where
+		baseSet = ctorNames $ getCtor hite (headNote "Train.Type.impliesReq" ctors)
+		
+
+
+simplifyReqs = bddSimplify impliesReq
+
+simplifyScopes = mapBDD f
+	where
+		f (Scope func xs) = if bddIsTrue res then bddTrue else bddLit $ Scope func res
+			where res = simplifyReqs xs
+
+
+impliesReq :: [(Req, Bool)] -> Req -> Maybe Bool
+impliesReq given req@(Req hite on path ctors) = 
+		if poss `subset` ctors then Just True
+		else if ctors `disjoint` poss then Just False
+		else Nothing
+	where
+		baseSet = ctorNames $ getCtor hite (headNote "Train.Type.impliesReq" ctors)
+		poss = foldr f baseSet given
+		
+		f (Req _ on2 path2 ctors2, False) poss
+			| on2 == on && path2 == path && finitePath path
+			= poss \\ ctors2
+		
+		f (Req _ on2 path2 ctors2,True) poss
+			| on2 == on && path `subsetPath` path2
+			= poss `intersect` ctors2
+
+		f _ poss = poss
+		
+impliesReq _ _ = Nothing
+
+
+
+reqNot :: Req -> Req
+reqNot (Req hite expr path ctors) = newReq hite expr path
+	(ctorNames (getCtor hite (headNote "Train.Type.reqNot" ctors)) \\ ctors)
+
+reqsNot :: Reqs -> Reqs
+reqsNot x = bddNot reqNot x
+
+instance Output Scope where
+	output (Scope name reqs) = "(\\forall " ++ name ++ ", " ++ output reqs ++ ")"
+
+instance Output a => Output (BDD a) where
+	output x = showBDDBy output x
+
+instance Output Req where
+	output (Req _ expr path ctor) =
+		output expr ++ output path ++ strSet ctor
+
--- src/Train/Type.hs
+++ src/Train/Type.hs
@@ -1,12 +1,10 @@
 
-module Train.Type(module Train.Path, module Train.Type) where
+module Train.Type(module Train.Path, module Train.Type, module Train.Req) where
 
-import Data.BDD
-import Data.List
-import Data.Char
 import Hite
 import General.General
 import Train.Path
+import Train.Req
 
 
 data ZHite = ZHite [Data] [ZFunc]
@@ -19,13 +17,6 @@ getZFunc (ZHite _ funcs) name = head [res | res@(ZFunc nam _ _) <- funcs, nam ==
 
 data ZFunc = ZFunc FuncName [FuncArg] [(Reqs, Either String ZExpr)]
 			 deriving Show
-
-data ZExpr = ZCall FuncName [ZExpr]
-		   | ZMake CtorName [ZExpr]
-		   | ZSel ZExpr CtorArg
-		   | ZVar FuncArg
-		   | ZAny
-		   deriving (Eq, Show, Ord)
 
 
 instance Manipulate ZExpr where
@@ -41,123 +32,3 @@ instance Manipulate ZExpr where
 		ZSel _ x -> let [y] = ys in ZSel y x
 		_ -> x
 
-
-
-type Scopes = BDD Scope
-data Scope = Scope FuncName Reqs
-			 deriving (Eq, Ord, Show)
-
-
-type Reqs = BDD Req
-data Req = Req Hite ZExpr Path [CtorName]
-		   deriving (Show)
-
-instance Eq Req where
-	(Req _ a1 b1 c1) == (Req _ a2 b2 c2) = a1 == a2 && b1 == b2 && c1 == c2
-
-instance Ord Req where
-	compare (Req _ a1 b1 c1) (Req _ a2 b2 c2) = compare (a1,b1,c1) (a2,b2,c2)
-
-
-newReq :: Hite -> ZExpr -> Path -> [CtorName] -> Req
-newReq hite zexpr path ctors
-	| path == newPath hite ["tl"] && ctors == ["[]"] = Req hite zexpr (emptyPath hite) ctors
-	| otherwise = Req hite zexpr path (nub $ sort ctors)
-
-newReqs :: Hite -> ZExpr -> Path -> [CtorName] -> Reqs
-newReqs hite zexpr path ctors | null ctors = bddFalse
-							  | ctors `setEq` baseSet = bddTrue
-							  | otherwise = bddLit $ newReq hite zexpr path ctors
-	where
-		baseSet = ctorNames $ getCtor hite (headNote "Train.Type.impliesReq" ctors)
-		
-
-
-simplifyReqs = bddSimplify impliesReq
-
-simplifyScopes = mapBDD f
-	where
-		f (Scope func xs) = if bddIsTrue res then bddTrue else bddLit $ Scope func res
-			where res = simplifyReqs xs
-
-
-impliesReq :: [(Req, Bool)] -> Req -> Maybe Bool
-impliesReq given req@(Req hite on path ctors) = 
-		if poss `subset` ctors then Just True
-		else if ctors `disjoint` poss then Just False
-		else Nothing
-	where
-		baseSet = ctorNames $ getCtor hite (headNote "Train.Type.impliesReq" ctors)
-		poss = foldr f baseSet given
-		
-		f (Req _ on2 path2 ctors2, False) poss
-			| on2 == on && path2 == path && finitePath path
-			= poss \\ ctors2
-		
-		f (Req _ on2 path2 ctors2,True) poss
-			| on2 == on && path `subsetPath` path2
-			= poss `intersect` ctors2
-
-		f _ poss = poss
-		
-impliesReq _ _ = Nothing
-
-{-
-instance PredLit Req where
-	a ?=> b = a == b
-	
-	(Req _ a1 b1 c1) ?/\ (Req _ a2 b2 c2)
-		| a1 == a2 && nullPath b1 && nullPath b2 && null (c1 `intersect` c2) = Value False
-	_ ?/\ _ = Same
-
-
-instance PredLit Scope where
-	a ?=> b = a == b
-	
-	(Scope a1 b1) ?/\ (Scope a2 b2)
-		| a1 == a2 = Single $ Scope a1 (predAnd [b1,b2])
-		| otherwise = Same
-	
-	simp (Scope a b) | isTrue b = Just True
-					 | otherwise = Nothing
--}
-
-{-
-instance BDDLit Req where
-	litNot (Req hite expr path ctors) = bddLit $ Req hite expr path (getCtorsFromCtor hite (head ctors) \\ ctors)
-
-instance BDDLit Scope where
-	litNot (Scope name reqs) = bddLit $ Scope name (reqsNot reqs)
--}
-
-reqNot :: Req -> Req
-reqNot (Req hite expr path ctors) = newReq hite expr path
-	(ctorNames (getCtor hite (headNote "Train.Type.reqNot" ctors)) \\ ctors)
-
-reqsNot :: Reqs -> Reqs
-reqsNot x = bddNot reqNot x
-{-
-reqsNot x =  x = bddFalse
-		  | bddIsFalse x = bddTrue
-		  | otherwise = mapBDD (bddLit . reqNot) x
--}
-
-instance Output ZExpr where
-	output x = case x of
-		ZCall x [] -> x
-		ZCall x xs -> "(" ++ x ++ concatMap ((' ':) . output) xs ++ ")"
-		ZMake x xs -> output (ZCall x xs)
-		ZSel x y -> output x ++ "." ++ y
-		ZVar x -> x
-		ZAny -> "_"
-
-
-instance Output Scope where
-	output (Scope name reqs) = "(\\forall " ++ name ++ ", " ++ output reqs ++ ")"
-
-instance Output a => Output (BDD a) where
-	output x = showBDDBy output x
-
-instance Output Req where
-	output (Req _ expr path ctor) =
-		output expr ++ output path ++ strSet ctor
