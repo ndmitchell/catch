12aad658d5b8cc6a2ab87cbfd181646628b54758
Neil Mitchell <ndmitchell@gmail.com>
2006-09-11 22:11:00 +0000

    Make constructors have better names for selectors in some cases, tl, hd, tup2_1 etc

--- src/Convert/CoreHite.hs
+++ src/Convert/CoreHite.hs
@@ -95,7 +95,12 @@ convData (CoreData dname typ ctors) = Data dname (g [] ctors) typ
         f seen (CoreCtor cname args) = Ctor cname (zipWith g [1..] $ map snd args) (map (convType . fst) args)
             where
                 g n (Just x) = demandUnique seen x
-                g n (Nothing) = dname ++ "_" ++ cname ++ "_" ++ show n
+                g n (Nothing) | cname == "Prelude.:" = ["","hd","tl"] !! n
+                              | cname == "Prelude.1()" = "tup1_1"
+                              | "Prelude.(" `isPrefixOf` cname =
+                                        let i = length (filter (==',') cname) + 1
+                                        in "tup" ++ show i ++ "_" ++ show n
+                              | otherwise = dname ++ "_" ++ cname ++ "_" ++ show n
 
 		demandUnique seen x | x `notElem` seen = x
 							| otherwise = head [x2 | i <- [1..], let x2 = x ++ show i, x2 `notElem` seen]
