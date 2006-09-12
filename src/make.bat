a609ce177ac6b09319abc3e15049e68a0d6c1850
Neil Mitchell <ndmitchell@gmail.com>
2006-09-12 01:08:49 +0000

    Add the required makefile changes

--- src/make.bat
+++ src/make.bat
@@ -11,6 +11,7 @@ set flag=
 if %mode% == debug set flag=.exe -O0
 if %mode% == release set flag=.exe -O1
 if %mode% == profile set flag=_prof.exe -prof -auto-all
+if %mode% == drift goto drift
 
 if "%flag%" == "" goto bad_flags
 
@@ -21,6 +22,16 @@ goto finish
 
 :bad_flags
 echo Bad flag specified
+goto finish
+
+
+:drift
+echo module Hite.Binary where > Hite\Binary.hs
+echo import General.Binary >> Hite\Binary.hs
+echo import Hite.Type; import Hite.TypeType; import Hite.DataType >> Hite\Binary.hs
+drift -r Hite\Type.hs >> Hite\Binary.hs
+drift -r Hite\TypeType.hs >> Hite\Binary.hs
+drift -r Hite\DataType.hs >> Hite\Binary.hs
 goto finish
 
 
