c9890e5c34573bd125601592ff41bbc1a3160c1b
Neil Mitchell <ndmitchell@gmail.com>
2006-09-12 15:37:58 +0000

    Make RisersReal use custom read/show for int, since the default one pulls in lex!

--- src/Example/RisersReal.hs
+++ src/Example/RisersReal.hs
@@ -1,10 +1,12 @@
 
 module RisersReal where
 
+import Data.Char
+
 main :: IO ()
 main = do
     c <- getContents 
-    putStrLn $ show $ length $ risers $ takeWhile (/= 0) $ map read $ lines c
+    putStrLn $ showInt $ length $ risers $ takeWhile (/= 0) $ map readInt $ lines c
 
 
 risers :: Ord a => [a] -> [[a]]
@@ -13,3 +15,14 @@ risers [x] = [[x]]
 risers (x:y:etc) = if x <= y then (x:s):ss else [x]:(s:ss)
    where (s:ss) = risers (y:etc)
 
+
+
+readInt :: String -> Int
+readInt x = f 1 $ reverse x
+    where
+        f base (x:xs) = (digitToInt x * base) + (f (base*10) xs)
+        f base [] = 0
+
+showInt :: Int -> String
+showInt x | x < 10 = [intToDigit x]
+          | otherwise = showInt (x `div` 10) ++ [intToDigit (x `mod` 10)]
