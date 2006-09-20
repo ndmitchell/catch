ac2bfae4a475ddaaee314f6e9e9d0cd4007f2439
Neil Mitchell <ndmitchell@gmail.com>
2006-09-20 01:28:53 +0000

    Make the inlining more powerful, solves two problems in one

--- src/Transform/Specialise.hs
+++ src/Transform/Specialise.hs
@@ -89,8 +89,7 @@ inliner ihite = if res `eqFuncs` ihite then Nothing else Just res
 
         canInline (Func name _ body [(TweakExpr a,b)]) =
             case body of
-                (Cell nam 0 xs) | nam /= name && all isSimple xs -> True
-                (Cell nam n []) | nam /= name -> True
+                (Cell nam n xs) | nam /= name && all isSimple xs -> True
                 (Make ('(':_) xs) -> True
                 _ -> isSimple body
             where name = FuncPtr b a
