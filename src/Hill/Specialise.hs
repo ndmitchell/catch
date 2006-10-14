fd7c425a109e72d5cacd20f0742adbb193ab21c0
Neil Mitchell <ndmitchell@gmail.com>
2006-10-14 22:17:12 +0000

    Make specialisation introduce the lambda's first

--- src/Hill/Specialise.hs
+++ src/Hill/Specialise.hs
@@ -20,10 +20,11 @@ cmdsSpecialise = [Action "hill-specialise" specialise]
 ---------------------------------------------------------------------
 
 specialise :: CmdLineState -> String -> ValueHill -> IO ValueHill
-specialise state _ (ValueHill hill) = do
+specialise state _ (ValueHill hillBad) = do
         hPutStrLn (cmdLineHandle state) $ showTemplate template
         return $ ValueHill $ makeCode hill template
     where
+        hill = addLambdas hillBad
         template = filterTemplate $ makeTemplate hill
 
 
