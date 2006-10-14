922c646cf6ea87e82ca7340d928288b056fb8f9b
Neil Mitchell <ndmitchell@gmail.com>
2006-10-14 21:31:25 +0000

    Lift lambda (lambda x) to just one lambda x

--- src/Hill/Lambdas.hs
+++ src/Hill/Lambdas.hs
@@ -19,8 +19,10 @@ addLambdas hill = mapOverHill f hill
 moveLambdas :: ManipulateHill hill => hill -> hill
 moveLambdas hill = mapOverHill f hill
     where
-        f lhs@(Apply (Lambda n x) xs) = mkApply (mkLambda (n - length a) (Apply x a)) b
+        f (Apply (Lambda n x) xs) = mkApply (mkLambda (n - length a) (Apply x a)) b
             where (a,b) = splitAt n xs
+        
+        f (Lambda n (Lambda m x)) = mkLambda (n+m) x
         
         f (Case on alts) = mkLambda arity (Case on [alt{altExpr = dropLambda (altExpr alt)} | alt <- alts])
             where arity = maximum (0:[n | Lambda n _ <- map altExpr alts])
