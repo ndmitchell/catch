f3d58e588d8dd17d5f93a2dd30b470382d16cce4
Neil Mitchell <ndmitchell@gmail.com>
2006-10-31 01:00:02 +0000

    Add coreMake functionality, which just invokes Yhc

--- src/Hill/File.hs
+++ src/Hill/File.hs
@@ -5,17 +5,22 @@ import Hill.Type
 import Hill.Cache
 import Front.CmdLine
 import Control.Monad
+import System.FilePath
+import System
 
 
 cmdsFile =
-    [-- Action "core-make" coreMake
+    [Action "core-make" coreMake
      -- ,Action "core-load" coreLoad
      -- ,Action "core-save" coreSave
-     Action "hill-load" hillLoad
+    ,Action "hill-load" hillLoad
     ,Action "hill-save" hillSave
     ]
 
 
+
+coreMake :: CmdLineState -> String -> Value -> IO Value
+coreMake state _ (ValueFile x) = system ("yhc -hide -linkcore " ++ x) >> return (ValueFile x)
 
 
 hillLoad :: CmdLineState -> String -> Value -> IO Value
