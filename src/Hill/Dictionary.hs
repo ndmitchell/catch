0f35bf207ad35ff7b106a73acc3e119a97c7c3be
Neil Mitchell <ndmitchell@gmail.com>
2006-10-31 19:20:58 +0000

    Rewrite the dictionary stuff, now more robust - handles things like nested dictionaries for [] etc

--- src/Hill/Dictionary.hs
+++ src/Hill/Dictionary.hs
@@ -12,82 +12,81 @@ import General.General
 cmdsDictionary = [hillCmdPure "remove-dict" (const removeDictionary)]
 
 
+data TypeValue = TypeValue {typeValueModu :: String, val :: String}
+                 deriving (Show, Eq, Ord)
 
-data Dict = Dict {
-                dictImp :: String,
-                dictClass :: String,
-                dictClassMod :: String,
-                dictType :: String,
-                dictTypeMod :: String
-            }
+data TypeClass = TypeClass {typeClassModu :: String, cls :: String}
+                 deriving (Show, Eq, Ord)
 
-
-
-
--- a
-
-
-
+data Dictionary = Dictionary {dictName :: String, dictModu :: String
+                             ,typVal :: TypeValue, typCls :: TypeClass}
+                  deriving (Show, Eq, Ord)
 
 
 removeDictionary :: Hill -> Hill
-removeDictionary hill = hill{datas = newdatas ++ datas hill, funcs = map usedatas (newfuncs ++ filter elimold (funcs hill))}
+removeDictionary hill = hill{datas = newdatas ++ datas hill, funcs = newfuncs ++ filter elimold (funcs hill)}
     where
         dicts = getDictionaries hill
-        classes = groupSetExtract dictClass dicts
-        
-        newdatas = map f classes
-            where
-                f xs@(x:_) = Data ("Dict" ++ dictClass x) (map g xs) []
-                g x = Ctor (dictClass x ++ dictType x) [] []
 
-        newpairs = concatMap f dicts
-            where
-                f x = zipWith (\n a -> ((cls,n),a)) [0..] members
-                    where
-                        members = getMembers $ getFunc hill $ dictImp x
-                        cls = dictClass x ++ dictType x
-                        
-        getMembers (Func _ _ (Apply _ xs)) = map fromFun xs
+        typeFree (TypeValue modu val) =
+            length $ concat [free | Data x _ free <- datas hill, x == modu ++ "." ++ val]
+
         
-        newfuncs = concatMap (f . head) classes
+        newdatas = [Data "Type" ctrs []]
             where
-                f x = zipWith (g x) [0..] $ getMembers $ getFunc hill $ dictImp x
+                ctrs = map f $ snub $ map typVal dicts
+            
+                f t@(TypeValue modu val) = Ctor ("Type" ++ val)
+                             ["type" ++ val ++ show i | i <- [1..fre]]
+                             (replicate fre (TyCon "Type" []))
+                    where
+                        fre = typeFree t
+        
+        newfuncs = newdicts ++ newaccess
+        
+        newdicts = map f dicts
+            where
+                f dict = Func (dictName dict) frees (Apply (Ctr $ "Type" ++ val (typVal dict)) (map Var frees))
+                    where frees = [1..typeFree (typVal dict)]
+        
+        newaccess = concatMap f $ groupSetExtract typCls dicts
+            where
+                f xs@(x:_) = map (g (typCls x) xs) (zip [0..] members)
+                    where
+                        members = map (reverse . takeWhile (/= '.') . reverse) $ getMembers $ getFunc hill $ dictName x
                 
-                g x pos member = Func name [0] (Case (Var 0) alts)
+                g typcls xs (n,member) = Func (typeClassModu typcls ++ "." ++ member) [0] (Case (Var 0) alts)
                     where
-                        memname = reverse $ takeWhile (/= '.') $ reverse member
-                        name = if isUpper (head memname)
-                               then concat $ intersperse "." (init (splitList "." (dictImp x)) ++ [splitList "." member !! 2])
-                               else dictClassMod x ++ "." ++ memname
-                        alts = [AltCtr a (Fun c) | ((a,b),c) <- newpairs, b == pos, a == dictClass x ++ dictType x]
-
+                        alts = [AltCtr
+                                    ("Type" ++ tstr)
+                                    (Apply (Fun $ dictName x ++ "." ++ member) (map (\i -> Var 0 `Sel` ("type" ++ tstr ++ show i)) [1..typeFree (typVal x)]))
+                                    | x <- xs, let tstr = val (typVal x)]
+        
+        getMembers (Func _ _ (Apply _ xs)) = map fromFun xs
         
         elimold x = not $ funcName x `elem` map funcName newfuncs
 
-        usedatas x = mapOverHill f x
-            where
-                f (Fun y) = case [d | d <- dicts, dictImp d == y] of
-                                [] -> Fun y
-                                [x] -> Ctr (dictClass x ++ dictType x)
-                f x = x
 
 
-
-
-getDictionaries :: Hill -> [Dict]
+getDictionaries :: Hill -> [Dictionary]
 getDictionaries hill = concatMap f (funcs hill)
     where
-        f (Func name args body) | isDictionary body =
-                if length nams == 5 then [Dict name (nams !! 2) (nams !! 1) (nams !! 4) (nams !! 3)]
-                else if length nams == 7 then [Dict name (nams !! 3) (nams !! 2) (nams !! 6) ((nams !! 4) ++ "." ++ (nams !! 5))]
-                else error $ show ("Hill.Dictionary.getDictionaries",name)
+        f (Func name args body) | isDictionary args body =
+                case length nams of
+                    5 -> [Dictionary name (nams!!0) (TypeValue (nams!!3) (nams!!4)) (TypeClass (nams!!1) (nams!!2))]
+                    -- 7 -> [Dictionary (nams!!0 ++ "." ++ nams!!1) (TypeValue (nams!!3) (nams!!4)) (TypeClass (nams!!1) (nams!!2))]
+                    _ -> error $ show ("Hill.Dictionary.getDictionaries",name)
             where
                 nams = splitList "." name
+                
+                
         f _ = []
         
-        isDictionary (Apply (Ctr tup) xs) = isTuple tup && all isFun xs
-        isDictionary _ = False
+        isDictionary args (Apply (Ctr tup) xs) = isTuple tup && all (isFunCall args) xs
+        isDictionary args _ = False
+        
+        isFunCall args (Apply (Fun x) vars) = and $ zipWith (==) vars (map Var [0..])
+        isFunCall args x = isFun x
 
 
         isTuple x | "Prelude." `isPrefixOf` x = isTuple $ drop 8 x
