4c3cc5bc11683a42267ab276ad5b19ea31d33697
Neil Mitchell <ndmitchell@gmail.com>
2006-11-15 02:03:45 +0000

    Add Proposition.All, all the propositions at once, for better error checking

---
+++ proposition/Data/Proposition/All.hs
@@ -0,0 +1,46 @@
+
+module Data.Proposition.All(PropAll(..)) where
+
+import Data.Proposition.Internal
+import Data.Proposition.Simple
+import Data.Proposition.Formula
+import Data.Proposition.BDD
+
+import Safe
+import Control.Monad
+
+
+data PropAll a = PropAll {propSimple :: PropSimple a, propFormula :: Formula a, propBDD :: BDD a}
+
+
+instance Show a => Show (PropAll a) where
+    show = show . propSimple
+
+
+instance Prop PropAll where
+    propTrue  = PropAll propTrue  propTrue  propTrue
+    propFalse = PropAll propFalse propFalse propFalse
+    
+    propIsTrue (PropAll a b c) =
+        if propIsTrue a
+        then assertNote "PropAll.propIsTrue" (propIsTrue b && propIsTrue c) True
+        else False
+
+    propIsFalse (PropAll a b c) =
+        if propIsFalse a
+        then assertNote "PropAll.propIsFalse" (propIsFalse b && propIsFalse c) True
+        else False
+    
+    propLit a = PropAll (propLit a) (propLit a) (propLit a)
+    propAnd (PropAll a1 b1 c1) (PropAll a2 b2 c2) = PropAll (propAnd a1 a2) (propAnd b1 b2) (propAnd c1 c2)
+    propOr  (PropAll a1 b1 c1) (PropAll a2 b2 c2) = PropAll (propOr  a1 a2) (propOr  b1 b2) (propOr  c1 c2)
+    
+    propNot (PropAll a b c) = PropAll (propNot a) (propNot b) (propNot c)
+    
+    propMapM f (PropAll a b c) = do
+        a2 <- propMapM (liftM propSimple  . f) a
+        b2 <- propMapM (liftM propFormula . f) b
+        c2 <- propMapM (liftM propBDD     . f) c
+        return $ PropAll a2 b2 c2
+
+    propRebuild = propRebuild . propSimple
