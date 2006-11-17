88336aa922c2e0fd3dd1ba8b200be915004a1f34
Neil Mitchell <ndmitchell@gmail.com>
2006-11-17 23:37:09 +0000

    Delete commented out lines, before doing a bit of refactoring

--- src/Tram/Backward.hs
+++ src/Tram/Backward.hs
@@ -24,13 +24,10 @@ backward hill template hndl x = do
         origMap = Map.fromList [(a,b) | Scope a b <- x]
         origTodo = Map.keys origMap
     
-		--outBoth x = hPutStrLn hndl x >> putStrLn x
-        
         f :: Map.Map FuncName Reqs -> [FuncName] -> IO (Map.Map FuncName Reqs)
         f table [] = return table
         f table (x:xs) = do
             hPutStrLn hndl $ "Processing: " ++ x
-            -- () <- if x == "taut_let" then error $ unlines $ map show $ propagate hill $ Scope x $ fromJust $ Map.lookup x table else return ()
             res <- oneStep $ Scope x $ fromJust $ Map.lookup x table
             
             let (todo2,table2) = foldr g ([], table) res
@@ -58,34 +55,3 @@ backward hill template hndl x = do
         simple (Scope name x) = do
             x2 <- reducesWithM (templateGet template) $ simplifyReqs x
             return $ Scope name $ simplifyReqs x2
-            
-{-        
-        
-	
-		f scope = do
-			scope2 <- backs scope
-			fixp propTrue g scope2
-
-		g (Scope "main" x) gen = return [Scope "main" x]
-
-		g scope gen = do
-			let scopes = propagate hill scope
-			res <- mapM (\x -> backs x >>= gen) scopes
-			putStrLn $ "Propagate: " ++ show scope ++ " ==> " ++ show res
-			return res
-		
-		backs (Scope name x) = do
-			x2 <- reducesWithM (templateGet template) x
-			return (Scope name x2)
-	-}		
-{-
--- solve all but propagate
-backwardReqs :: Hill -> Template -> Handle -> Reqs -> IO Reqs
-backwardReqs hill template hndl x = mapPredLitM f x
-	where
-		f x = mapPredLitM g (reduce x)
-		
-		g req@(Req hite expr path ctor) = case expr of
-			Var y -> return $ predLit req
-			Call name xs -> templateGet template req
--}
