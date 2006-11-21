607bbf31586ecfd43aedbca9230b3e0967c1f937
Neil Mitchell <ndmitchell@gmail.com>
2006-11-21 19:18:40 +0000

    Move the data structure over to Proposition.BDD, now everything is in the right place

--- proposition/Data/Proposition/BDD.hs
+++ proposition/Data/Proposition/BDD.hs
@@ -2,8 +2,13 @@
 module Data.Proposition.BDD(BDD) where
 
 import Data.Proposition.Internal
-import Data.BDD
 import qualified Data.Map as Map
+
+
+data BDD a = AtomTrue
+           | AtomFalse
+           | Choice a (BDD a) (BDD a) -- false, true
+           deriving (Eq, Ord)
 
 
 instance Prop BDD where
