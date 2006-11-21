1c188c92a41930c525f6592090252f9e725c31a1
Neil Mitchell <ndmitchell@gmail.com>
2006-11-21 18:42:14 +0000

    Rewrite propAnd/propOr for BDD's, make it more uniform

--- proposition/Data/BDD.hs
+++ proposition/Data/BDD.hs
@@ -39,39 +39,6 @@ bddApplyAnd merge = rebalance . f
 
 
 
-bddNot :: (Show a, Ord a) => BDD a -> BDD a
-bddNot AtomTrue  = AtomFalse
-bddNot AtomFalse = AtomTrue
-bddNot (Choice a f t) = Choice a (bddNot f) (bddNot t)
-
-
-bddAnd :: Ord a => BDD a -> BDD a -> BDD a
-bddAnd AtomTrue b = b
-bddAnd b AtomTrue = b
-bddAnd AtomFalse b = AtomFalse
-bddAnd b AtomFalse = AtomFalse
-bddAnd a b = mergeWith bddAnd a b
-
-
-bddOr :: Ord a => BDD a -> BDD a -> BDD a
-bddOr AtomFalse b = b
-bddOr b AtomFalse = b
-bddOr AtomTrue b = AtomTrue
-bddOr b AtomTrue = AtomTrue
-bddOr a b = mergeWith bddOr a b
-
-
-mergeWith :: Ord a => (BDD a -> BDD a -> BDD a) -> BDD a -> BDD a -> BDD a
-mergeWith f c1 c2 | not (bddValid c1) || not (bddValid c2) = error "Precond failed"
-mergeWith f c1@(Choice a1 f1 t1) c2@(Choice a2 f2 t2) = 
-	if bddValid res then res else error $ "Postcond failed" ++ show (compare a1 a2)
- where
-  res =
-	case compare a1 a2 of
-		EQ -> choice a1 (f f1 f2) (f t1 t2)
-		LT -> choice a1 (f f1 c2) (f t1 c2)
-		GT -> choice a2 (f c1 f2) (f c1 t2)
-
 choice a t f = if t == f then t else Choice a t f
 
 
--- proposition/Data/Proposition/BDD.hs
+++ proposition/Data/Proposition/BDD.hs
@@ -13,9 +13,12 @@ instance Prop BDD where
 
     propLit a = Choice a AtomFalse AtomTrue
 
-    propAnd = bddAnd
-    propOr  = bddOr
-    propNot = bddNot
+    propNot AtomTrue  = AtomFalse
+    propNot AtomFalse = AtomTrue
+    propNot (Choice a f t) = Choice a (propNot f) (propNot t)
+
+    propAnd = mergeWith propIsTrue  propIsFalse 
+    propOr  = mergeWith propIsFalse propIsTrue
 
     propMapM = mapBDDM
 
@@ -24,3 +27,22 @@ instance Prop BDD where
             liftAnd a b = case a ?/\ b of
                                Value x -> Just x
                                _ -> Nothing
+
+
+---------------------------------------------------------------------
+-- MERGING, FOR OR/AND
+
+mergeWith :: Ord a => (BDD a -> Bool) -> (BDD a -> Bool) -> BDD a -> BDD a -> BDD a
+mergeWith ignore promote c1 c2
+    | ignore c1 = c2
+    | ignore c2 = c1
+    | promote c1 = c1
+    | promote c2 = c2
+
+mergeWith ignore promote c1@(Choice a1 f1 t1) c2@(Choice a2 f2 t2) =
+    case compare a1 a2 of
+        EQ -> choice a1 (cont f1 f2) (cont t1 t2)
+        LT -> choice a1 (cont f1 c2) (cont t1 c2)
+        GT -> choice a2 (cont c1 f2) (cont c1 t2)
+    where
+        cont = mergeWith ignore promote
