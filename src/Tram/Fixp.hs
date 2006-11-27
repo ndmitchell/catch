53a208bed3b6cd895b5ea1a17ec40cc56eaa27b1
Neil Mitchell <ndmitchell@gmail.com>
2006-11-27 00:39:57 +0000

    Change tabs to spaces in Fixp

--- src/Tram/Fixp.hs
+++ src/Tram/Fixp.hs
@@ -10,21 +10,21 @@ import Control.Monad
 
 fixp :: (Show v, Show k, Eq v, Eq k) => v -> (k -> (k -> IO v) -> IO v) -> k -> IO v
 fixp def solve key = gen [] key
-	where
-		-- look up in the stack if possible
-		ask stack key = do
-			-- putStrLn $ "Searching for: " ++ show key
-			-- putStrLn $ "In: " ++ show [(show a, show b) | (a,b) <- stack]
-			case lookup key stack of
-				Nothing -> {- putStrLn "Not found" >> -} gen stack key
-				Just x -> {- putStrLn "Found" >> -} return x
-		
-		-- create afresh
-		gen stack key = f def
-			where
-				f value = do
-					value2 <- solve key (ask ((key,value):stack))
-					if value == value2 then return value else f value2
+    where
+        -- look up in the stack if possible
+        ask stack key = do
+            -- putStrLn $ "Searching for: " ++ show key
+            -- putStrLn $ "In: " ++ show [(show a, show b) | (a,b) <- stack]
+            case lookup key stack of
+                Nothing -> {- putStrLn "Not found" >> -} gen stack key
+                Just x -> {- putStrLn "Found" >> -} return x
+        
+        -- create afresh
+        gen stack key = f def
+            where
+                f value = do
+                    value2 <- solve key (ask ((key,value):stack))
+                    if value == value2 then return value else f value2
 
 
 -- REQ SPECIFIC PART
