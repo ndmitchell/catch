24c9e835997cab0f44cbf01e5a0809d57d1e055f
Neil Mitchell <ndmitchell@gmail.com>
2007-01-02 22:58:47 +0000

    Firstifier, that can work properly on the Defunc example

--- firstify/Main.hs
+++ firstify/Main.hs
@@ -48,7 +48,7 @@ firstify x = f 10 emptySpec x
                 | n == 0 = (False, x)
                 | otherwise = uncurry (f (n-1)) (process s x)
 
-        process spec core = (spec2, core2)
+        process spec core = (spec2, core3)
             where
                 core3 = coreReachable ["main"] $ coreSimplify core2
                 (spec2,core2) = specHO spec $ coreSimplify $ inlineHO core
@@ -84,9 +84,79 @@ inlineHO core = mapUnderCore f $ zeroApp core
 
 
 
-data Special = Special [(String,[CoreExpr],String)]
+type Special = [Spec]
+data Spec = Spec String [CoreExpr] String
+            deriving Show
 
-emptySpec = Special []
+instance Eq Spec where
+    (Spec a b _) == (Spec c d _) = a == c && b == d
+
+
+emptySpec = []
 
 specHO :: Special -> Core -> (Special, Core)
-specHO spec core = (spec, core)
+specHO spec core = (spec2, core3)
+    where
+        core3 = useSpecial core2 spec2
+        core2 = core{coreFuncs = func2 ++ coreFuncs core}
+        func2 = genSpecial core new
+        spec2 = new ++ spec
+        new = nameSpecial core $ askSpecial core \\ spec
+
+
+
+
+
+askSpecial :: Core -> [Spec]
+askSpecial core = nub $ mapMaybe (wantSpecial core) $ allCore core
+
+
+wantSpecial :: Core -> CoreExpr -> Maybe Spec
+wantSpecial core (CoreApp (CoreFun x) xs) | any (isHO core) xs = Just $ f x xs
+    where
+        f x xs = Spec x xs2 ""
+            where CoreApp _ xs2 = g (CoreApp (CoreFun x) (zipWith h [0..] xs))
+
+        h n x = if isHO core x then replaceFreeVars rep x else CoreVar ('_':show n)
+            where rep = zip (collectFreeVars x) [CoreVar $ 'v':show n ++ "_" ++ show i | i <- [1..]]
+        
+        g = repFree ['v':show n | n <- [1..]] . repFree ['$':show n | n <- [1..]]
+        
+        repFree new x = replaceFreeVars (zip frees (map CoreVar freenew)) $ uniqueFreeVarsWith othernew x
+            where
+                (freenew,othernew) = splitAt (length frees) new
+                frees = collectFreeVars x
+wantSpecial _ _ = Nothing
+
+
+nameSpecial :: Core -> [Spec] -> [Spec]
+nameSpecial core xs = f (map coreFuncName $ coreFuncs core) xs
+    where
+        f seen [] = []
+        f seen (Spec func args _ : rest) = Spec func args newname : f (newname:seen) rest
+            where newname = head $ [name | i <- [1..], let name = func ++ "_" ++ show i, name `notElem` seen]
+
+
+genSpecial :: Core -> [Spec] -> [CoreFunc]
+genSpecial core specs = map f specs
+    where
+        f (Spec func args name) = CoreFunc name (collectFreeVars (CoreApp (CoreFun func) args)) body2
+            where
+                CoreFunc _ params body = coreFunc core func
+                body2 = replaceFreeVars (zip params args) body
+
+
+useSpecial :: Core -> Special -> Core
+useSpecial core spec = mapUnderCore f core
+    where
+        f o@(CoreApp (CoreFun x) xs) | isJust ms && fromJust ms `elem` spec =
+                CoreApp (CoreFun name) (concatMap g xs)
+            where
+                Spec _ args name = head $ filter (==fromJust ms) spec
+                ms = wantSpecial core o
+                
+                g x | isHO core x = map CoreVar $ collectFreeVars x
+                    | otherwise = [x]
+                
+        f x = x
+
