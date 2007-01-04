533804f7a7411567b16afabe84c25b4261261d56
Neil Mitchell <ndmitchell@gmail.com>
2007-01-04 23:01:41 +0000

    Fix the make file, so that ghc doesn't rebuild unnecessarily

--- firstify/make.bat
+++ firstify/make.bat
@@ -7,4 +7,4 @@ if exist D:\sources\yhc\h.o\yhc set comp=D:\sources\yhc\h.o\yhc
 if exist C:\Documents\Uni\yhc\15_sep_2006\yhc-devel set comp=C:\Documents\Uni\yhc\15_sep_2006\yhc-devel
 if exist C:\Documents\Uni\yhc\current set comp=C:\Documents\Uni\yhc\current
 
-ghc --make Main -o firstify -odir obj -hidir obj -i%comp%\src\libraries\core -i%comp%\src\libraries\general
+ghc --make Main -o firstify.exe -odir obj -hidir obj -i%comp%\src\libraries\core -i%comp%\src\libraries\general
