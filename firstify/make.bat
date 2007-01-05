98598a02c08a97850b0471777e72344f83a21b40
Neil Mitchell <ndmitchell@gmail.com>
2007-01-05 13:21:31 +0000

    Change to use the correct location for the Yhc libraries

--- firstify/make.bat
+++ firstify/make.bat
@@ -2,9 +2,7 @@
 mkdir obj 2> nul
 
 if exist C:\Neil\yhc set comp=C:\Neil\yhc
-if exist D:\sources\yhc\11_sep_2006\yhc-devel set comp=D:\sources\yhc\11_sep_2006\yhc-devel
-if exist D:\sources\yhc\h.o\yhc set comp=D:\sources\yhc\h.o\yhc
-if exist C:\Documents\Uni\yhc\15_sep_2006\yhc-devel set comp=C:\Documents\Uni\yhc\15_sep_2006\yhc-devel
 if exist C:\Documents\Uni\yhc\current set comp=C:\Documents\Uni\yhc\current
+if exist D:\sources\yhc\current set comp=D:\sources\yhc\current
 
 ghc --make Main -o firstify.exe -odir obj -hidir obj -i%comp%\src\libraries\core -i%comp%\src\libraries\general
