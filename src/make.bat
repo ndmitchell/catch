0e9c7ca88ed8a764bc12479d64a1f4d649a557ff
Neil Mitchell <ndmitchell@gmail.com>
2007-01-10 15:46:47 +0000

    Add another place to search for the library

--- src/make.bat
+++ src/make.bat
@@ -23,6 +23,7 @@ if exist C:\Neil\yhc set comp=C:\Neil\yhc
 if exist D:\sources\yhc\11_sep_2006\yhc-devel set comp=D:\sources\yhc\11_sep_2006\yhc-devel
 if exist D:\sources\yhc\h.o\yhc set comp=D:\sources\yhc\h.o\yhc
 if exist C:\Documents\Uni\yhc\15_sep_2006\yhc-devel set comp=C:\Documents\Uni\yhc\15_sep_2006\yhc-devel
+if exist E:\Neil\yhc set comp=E:\Neil\yhc
 
 echo Compiling for %mode%
 ghc --make Main -o catch%flag% -odir %out%\%mode% -hidir %out%\%mode% -i%comp%\src\libraries\core -i%comp%\src\libraries\general -i..\proposition
