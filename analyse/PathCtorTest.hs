7f641c75aa2a3c7d74f5ae30db76eabc251e5f55
Neil Mitchell <ndmitchell@gmail.com>
2007-01-19 23:15:20 +0000

    Add a new property (confluent_newPathCtor), along with some refactorisations

--- analyse/PathCtorTest.hs
+++ analyse/PathCtorTest.hs
@@ -9,9 +9,15 @@ import SmallCheck
 normalPath (Left x) = if x then truePathCtor else falsePathCtor
 normalPath (Right x) = x
 
+rePathCtor (PathCtor core path ctors) = newPathCtor core path ctors
+
 
 
 correct_newPathCtor :: PathCtor -> Bool
-correct_newPathCtor orig@(PathCtor core path ctors) =
+correct_newPathCtor orig =
         if equalPathCtor orig new then True else error $ show (orig, new)
-    where new = normalPath $ newPathCtor core path ctors
+    where new = normalPath $ rePathCtor orig
+
+
+confluent_newPathCtor :: PathCtor -> PathCtor -> Property
+confluent_newPathCtor a b = equalPathCtor a b ==> rePathCtor a == rePathCtor b
