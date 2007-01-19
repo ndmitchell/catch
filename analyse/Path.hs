c2ee54931850dbe0183c8295e5fa7655bfd734d4
Neil Mitchell <ndmitchell@gmail.com>
2007-01-19 18:55:13 +0000

    Remove Core from Path, was never really used

--- analyse/Path.hs
+++ analyse/Path.hs
@@ -12,13 +12,13 @@ import DataRep
 import Control.Monad
 
 
-data Path = Path Core [PathElem]
+data Path = Path [PathElem]
 
 instance Eq Path where
-	(Path _ a) == (Path _ b) = a == b
+	(Path a) == (Path b) = a == b
 
 instance Ord Path where
-	compare (Path _ a) (Path _ b) = compare (f a, a) (f b, b)
+	compare (Path a) (Path b) = compare (f a, a) (f b, b)
 		where
 			f xs = (length atm, 0 - length str, 0 - length (concat [x | PathStar x <- xs]))
 				where (str,atm) = partition isPathStar xs
@@ -29,7 +29,7 @@ data PathElem = PathAtom String
 
 
 instance Show Path where
-	show (Path _ xs) = concatMap (('.':) . show) xs
+	show (Path xs) = concatMap (('.':) . show) xs
 
 instance Show PathElem where
 	show (PathAtom x) = x
@@ -41,27 +41,27 @@ isPathStar (PathStar{}) = True ; isPathStar _ = False
 isPathAtom = not . isPathStar
 
 
-pathCtorArgs (Path _ x) = snub $ concatMap f x
+pathCtorArgs (Path x) = snub $ concatMap f x
     where
         f (PathAtom x) = [x]
         f (PathStar xs) = xs
 
 
-nullPath (Path _ x) = null x
+nullPath (Path x) = null x
 
-emptyPath hite = Path hite []
+emptyPath _ = Path []
 
 newPath hite xs = foldl integrate (emptyPath hite) xs
 
 
-ewpPath (Path _ x) = all isPathStar x
+ewpPath (Path x) = all isPathStar x
 
-finitePath (Path _ x) = all (not . isPathStar) x
-makeFinitePath (Path hite x) = Path hite $ filter (not . isPathStar) x
+finitePath (Path x) = all (not . isPathStar) x
+makeFinitePath (Path x) = Path $ filter (not . isPathStar) x
 
 
 restrictPath :: Path -> [CoreFieldName] -> Path
-restrictPath (Path h x) allow = Path h (concatMap f x)
+restrictPath (Path x) allow = Path (concatMap f x)
     where
         f (PathStar x) = if null x2 then [] else [PathStar x2]
             where x2 = filter (`elem` allow) x
@@ -69,7 +69,7 @@ restrictPath (Path h x) allow = Path h (concatMap f x)
 
 
 differentiate :: Path -> CoreFieldName -> Maybe Path
-differentiate (Path hite xs) ctor = liftM (Path hite) $ f xs
+differentiate (Path xs) ctor = liftM Path $ f xs
 	where
 		f [] = Nothing
 		f (PathAtom x:xs) | ctor == x = Just xs
@@ -81,13 +81,13 @@ differentiate (Path hite xs) ctor = liftM (Path hite) $ f xs
 -- Just integrate raw for now, blurPath will change to star's etc
 -- Do not expect simplification rules to be fired until AFTER blurPath
 integrate :: Path -> CoreFieldName -> Path
-integrate (Path hite x) ctor = Path hite (PathAtom ctor : x)
+integrate (Path x) ctor = Path (PathAtom ctor : x)
 
 
 
 -- is the first a subset of the second
 subsetPath :: Path -> Path -> Bool
-subsetPath (Path _ a) (Path _ b) = f a b
+subsetPath (Path a) (Path b) = f a b
 	where
 		f (PathAtom x:xs) (PathAtom y:ys) | x == y = f xs ys
 		f (PathStar x:xs) (PathStar y:ys) | x `subset` y = f xs ys
@@ -99,9 +99,9 @@ subsetPath (Path _ a) (Path _ b) = f a b
 -- blur paths are required
 -- the result must be a superset of the input
 blurPath :: Core -> Path -> Path
-blurPath hill (Path hite x) = Path hite (combineSucc $ map useStar x)
+blurPath core (Path x) = Path $ combineSucc $ map useStar x
     where
-        useStar (PathAtom ctor) | isFieldRecursive hite ctor = PathStar [ctor]
+        useStar (PathAtom ctor) | isFieldRecursive core ctor = PathStar [ctor]
         useStar x = x
     
         combineSucc (PathStar x1:PathStar x2:xs) = combineSucc (PathStar (snub $ x1 ++ x2) : xs)
