19ab0a5045c7520e3030d17d1637f54f8bf0ca65
Neil Mitchell <ndmitchell@gmail.com>
2007-01-23 18:32:35 +0000

    For confluent_or output all failing cases

--- analyse/PathCtorTest.hs
+++ analyse/PathCtorTest.hs
@@ -5,7 +5,7 @@ import PathCtor
 import PathCtorEq
 import SmallCheck
 import Data.Proposition
-
+import Debug.Trace
 
 
 normalPath (Left x) = if x then truePathCtor else falsePathCtor
@@ -45,8 +45,9 @@ correct_or a b = isRight a2 && isRight b2 && res /= None ==>
 
 confluent_or :: PathCtor -> PathCtor -> PathCtor -> Property
 confluent_or a b c = isRight a2 && isRight b2 && equalPathCtorProp propBox lhs (box c) ==>
-                     res /= None && normalPath (rePathCtor c) == reducePath res
+                     if ans then True else trace (show (a2,b2,rePathCtor c)) True
     where
+        ans = res /= None && normalPath (rePathCtor c) == reducePath res
         (a2,b2) = (rePathCtor a, rePathCtor b)
         (Right a3,Right b3) = (a2,b2)
     
