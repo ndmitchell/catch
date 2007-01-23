3bdc1622ad2e3870e851f7817cc0865e5d1baf86
Neil Mitchell <ndmitchell@gmail.com>
2007-01-23 00:55:00 +0000

    Add enough code to get correctness of or being proven

--- analyse/PathCtorTest.hs
+++ analyse/PathCtorTest.hs
@@ -11,6 +11,10 @@ import Data.Proposition
 normalPath (Left x) = if x then truePathCtor else falsePathCtor
 normalPath (Right x) = x
 
+reducePath (Value x) = x
+reducePath (Literal x) = if x then truePathCtor else falsePathCtor
+
+
 rePathCtor (PathCtor core path ctors) = newPathCtor core path ctors
 
 
@@ -26,10 +30,27 @@ confluent_newPathCtor a b = equalPathCtor a b ==> rePathCtor a == rePathCtor b
 
 
 
+correct_or :: PathCtor -> PathCtor -> Property
+correct_or a b = isRight a2 && isRight b2 && res /= None ==>
+                 if equalPathCtorProp propBox lhs rhs then True else error $ show (a3,b3,res)
+    where
+        (a2,b2) = (rePathCtor a, rePathCtor b)
+        (Right a3,Right b3) = (a2,b2)
+    
+        lhs = propOr (box a3) (box b3)
+        rhs = box (reducePath res) :: Formula (PropBox PathCtor)
+        
+        res = a3 ?\/ b3
+
+
 -- put the items in a box, so normal simplification rules do not fire
-data PropBox a = PropBox a
+box x = propLit (PropBox x)
+data PropBox a = PropBox {propBox :: a}
 instance (Show a, Ord a) => PropLit (PropBox a)
 instance Show a => Show (PropBox a) where show (PropBox a) = show a
 instance Eq a => Eq (PropBox a) where (==) (PropBox a) (PropBox b) = (==) a b
 instance Ord a => Ord (PropBox a) where compare (PropBox a) (PropBox b) = compare a b
 
+
+
+isRight (Right{}) = True; isRight _ = False
