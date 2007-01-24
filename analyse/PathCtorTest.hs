51d070b9ec7ed7e97818fb52d4119283a158bac6
Neil Mitchell <ndmitchell@gmail.com>
2007-01-24 12:12:42 +0000

    Move to using PropSimple's not in a PropBox, since there are now no rules to worry about

--- analyse/PathCtorTest.hs
+++ analyse/PathCtorTest.hs
@@ -31,38 +31,28 @@ confluent_atom a b = equalPathCtor a b ==> rePathCtor a == rePathCtor b
 
 correct_or :: PathCtor -> PathCtor -> Property
 correct_or a b = isRight a2 && isRight b2 && res /= None ==>
-                 if equalPathCtorProp propBox lhs rhs then True else error $ show (a3,b3,res)
+                 if equalPathCtorProp id lhs rhs then True else error $ show (a3,b3,res)
     where
         (a2,b2) = (rePathCtor a, rePathCtor b)
         (Right a3,Right b3) = (a2,b2)
     
-        lhs = propOr (box a3) (box b3)
-        rhs = box (reducePath res) :: PropSimple (PropBox PathCtor)
+        lhs = propOr (propLit a3) (propLit b3)
+        rhs = propLit (reducePath res) :: PropSimple PathCtor
         
         res = a3 ?\/ b3
 
 
 confluent_or :: PathCtor -> PathCtor -> PathCtor -> Property
-confluent_or a b c = isRight a2 && isRight b2 && equalPathCtorProp propBox lhs (box c) ==>
+confluent_or a b c = isRight a2 && isRight b2 && equalPathCtorProp id lhs (propLit c) ==>
                      if ans then True else trace (show (a2,b2,rePathCtor c)) True
     where
         ans = res /= None && normalPath (rePathCtor c) == reducePath res
         (a2,b2) = (rePathCtor a, rePathCtor b)
         (Right a3,Right b3) = (a2,b2)
     
-        lhs = propOr (box a3) (box b3) :: PropSimple (PropBox PathCtor)
+        lhs = propOr (propLit a3) (propLit b3) :: PropSimple PathCtor
         
         res = a3 ?\/ b3
-
-
--- put the items in a box, so normal simplification rules do not fire
-box x = propLit (PropBox x)
-data PropBox a = PropBox {propBox :: a}
-instance (Show a, Ord a) => PropLit (PropBox a)
-instance Show a => Show (PropBox a) where show (PropBox a) = show a
-instance Eq a => Eq (PropBox a) where (==) (PropBox a) (PropBox b) = (==) a b
-instance Ord a => Ord (PropBox a) where compare (PropBox a) (PropBox b) = compare a b
-
 
 
 isRight (Right{}) = True; isRight _ = False
