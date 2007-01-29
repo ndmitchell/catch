9462496b3bca15ceb3595dca59da7a0d6f8dfbdf
Neil Mitchell <ndmitchell@gmail.com>
2007-01-29 14:01:13 +0000

    Add PathCtorEq, partly done, fully generic

--- analyse/PathCtorEq.hs
+++ analyse/PathCtorEq.hs
@@ -1,9 +1,9 @@
 
 module PathCtorEq(
-    Value, equalValue,
-    enumeratePathCtor, equalPathCtor,
-    enumeratePathCtorProp, equalPathCtorProp,
-    normalise
+    --Val, equalValue,
+    --enumeratePathCtor, equalPathCtor,
+    --enumeratePathCtorProp, equalPathCtorProp,
+    --normalise
     ) where
 
 import PathCtor
@@ -12,20 +12,55 @@ import General
 import Data.List
 import Data.Maybe
 import Data.Proposition
+import Yhc.Core
 
 
-data Value = A Value Value
-           | B Value
-           | C Value
-           | D
-           | Star
-           deriving (Show, Eq, Ord)
+data Val = Val {valCtor :: String, valFields :: [Val]}
+         | Star
+           deriving (Eq, Ord)
 
 
-allValue = [("A",A Star Star), ("B",B Star), ("C",C Star), ("D",D)]
+instance Show Val where
+    show Star = "*"
+    show (Val x xs) = ['('|b] ++ unwords (x:map show xs) ++ [')'|b]
+        where b = not $ null xs
 
 
-enumeratePathCtor :: PathCtor -> [Value]
+-- allValue = [("A",A Star Star), ("B",B Star), ("C",C Star), ("D",D)]
+
+
+enumeratePathCtor :: PathCtor -> [Val]
+enumeratePathCtor (PathCtor core (Path []) ctor) =
+    [Val c (replicate arity Star) | c <- ctor, let arity = length $ coreCtorFields $ coreCtor core c]
+
+enumeratePathCtor (PathCtor core (Path (PathAtom x:xs)) ctor) =
+        [Val name (replicate arity Star) | c <- coreDataCtors dat, x `notElem` map (fromJust . snd) (coreCtorFields c),
+                                           let name = coreCtorName c, let arity = length $ coreCtorFields c]
+        ++
+        [Val (coreCtorName ctr) children | children <- sequence (map (f . fromJust . snd) (coreCtorFields ctr))]
+    where
+        ctr = coreFieldCtor core x
+        dat = coreCtorData core (coreCtorName ctr)
+        
+        f field | field == x = enumeratePathCtor (PathCtor core (Path xs) ctor)
+                | otherwise  = [Star]
+
+enumeratePathCtor (PathCtor core (Path (PathStar x:xs)) ctor) = concatMap f base
+    where
+        f c = [Val (coreCtorName c) children | children <- sequence (map (g . fromJust . snd) (coreCtorFields c))]
+        
+        g p = g2 p (PathStar x:xs)
+        
+        g2 p (PathAtom x:xs) | p == x = enumeratePathCtor (PathCtor core (Path xs) ctor)
+        g2 p (PathStar x:xs) | p `elem` x = enumeratePathCtor (PathCtor core (Path xs) ctor)
+                             | otherwise = g2 p xs
+        g2 p _ = [Star]
+        
+        base = if null xs
+               then map (coreCtor core) ctor
+               else coreDataCtors $ coreFieldData core $ head x
+
+{-
 enumeratePathCtor (PathCtor core path ctor) = concatMap f base
     where
         f (A x y) = [A x2 y2 | x2 <- g "a", y2 <- g "as"]
@@ -43,8 +78,9 @@ enumeratePathCtor (PathCtor core path ctor) = concatMap f base
         base = if ewpPath path
                then if length ctor == 4 then [Star] else [b | (a,b) <- allValue, a `elem` ctor]
                else map snd allValue
+-}
 
-enumeratePathCtorProp :: Prop p => p PathCtor -> [Value]
+enumeratePathCtorProp :: Prop p => p PathCtor -> [Val]
 enumeratePathCtorProp p = propFold fold p
     where
         fold = PropFold {foldOr = ors, foldAnd = ands, foldLit = enumeratePathCtor}
@@ -59,19 +95,23 @@ enumeratePathCtorProp p = propFold fold p
 
 
 -- is a `subset` b
-subsetValue :: Value -> Value -> Bool
+subsetValue :: Val -> Val -> Bool
 subsetValue _ Star = True
-subsetValue (A a1 as1) (A a2 as2) = a1 `subsetValue` a2 && as1 `subsetValue` as2
-subsetValue (B bs1) (B bs2) = bs1 `subsetValue` bs2
-subsetValue (C c1) (C c2) = c1 `subsetValue` c2
-subsetValue D D = True
+subsetValue (Val a as) (Val b bs) = a == b && f as bs
+    where
+        f [] [] = True
+        f (x:xs) (y:ys) = x `subsetValue` y && f xs ys
+        f _ _ = False
 subsetValue _ _ = False
 
 
-normalise :: [Value] -> [Value]
+normalise :: [Val] -> [Val]
+normalise = undefined
+
+{-
 normalise xs = if Star `elem` xs || all ((`elem` res) . snd) allValue then [Star] else res
     where
-        res :: [Value]
+        res :: [Val]
         res = snub $ concat $ map (\x -> f (fst $ head x) (map snd x)) $
               groupSortBy cmpFst [(tagChar a,a) | a <- xs]
 
@@ -80,22 +120,22 @@ normalise xs = if Star `elem` xs || all ((`elem` res) . snd) allValue then [Star
         fromA (A a b) = (a,b)
         swap (a,b) = (b,a)
         
-        f :: Char -> [Value] -> [Value]
+        f :: Char -> [Val] -> [Val]
         f 'A' = map (uncurry A) . g . map fromA
         f 'B' = map B . normalise . map fromB
         f 'C' = map C . normalise . map fromC
         f 'D' = id
         
         
-        g :: [(Value, Value)] -> [(Value, Value)]
+        g :: [(Val, Val)] -> [(Val, Val)]
         g x = if x2 == x then x else g x2
             where x2 = snub $ map swap $ h $ map swap $ h x
         
-        h :: [(Value, Value)] -> [(Value,Value)]
+        h :: [(Val, Val)] -> [(Val,Val)]
         h = concat . map (\x -> map ((,) (fst $ head x)) $ normalise $ map snd x) . groupSortBy cmpFst
 
 
-normaliseMore :: [Value] -> [Value]
+normaliseMore :: [Val] -> [Val]
 normaliseMore xs = filter (\y -> not $ any (y `strictSubset`) xs) xs
     where strictSubset a b = a /= b && a `subsetValue` b
 
@@ -106,14 +146,8 @@ groupSortBy f = groupBy (\a b -> f a b == EQ) . sortBy f
 
 cmpFst (a,_) (b,_) = compare a b
 
-tagChar (A{}) = 'A'
-tagChar (B{}) = 'B'
-tagChar (C{}) = 'C'
-tagChar (D{}) = 'D'
 
-
-
-equalValue :: [Value] -> [Value] -> Bool
+equalValue :: [Val] -> [Val] -> Bool
 equalValue a b | a == b = True
                | na == nb = True
                | otherwise = normaliseMore na == normaliseMore nb
@@ -129,3 +163,4 @@ equalPathCtor a b = enumeratePathCtor a `equalValue` enumeratePathCtor b
 equalPathCtorProp :: Prop p => p PathCtor -> p PathCtor -> Bool
 equalPathCtorProp a b = enumeratePathCtorProp a `equalValue` enumeratePathCtorProp b
 
+-}
