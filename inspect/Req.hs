6184434ab67533ef9af5917debe9b8fc339922b9
Neil Mitchell <ndmitchell@gmail.com>
2007-01-31 14:59:52 +0000

    Initial version on Req, taking a guess at the new types for various things - operations can come later

---
+++ inspect/Req.hs
@@ -0,0 +1,29 @@
+
+module Req(module Val, Scopes, Scope(..)) where
+
+import Val
+import Yhc.Core
+import Data.Proposition
+
+
+-- DATA DEFINITIONS
+
+type Scopes = [Scope]
+data Scope = Scope CoreFuncName Vals
+
+type Reqs = PropSimple Req
+
+data Req = Req CoreExpr Vals
+         | Demonic
+         | Angelic
+         deriving (Ord, Eq)
+
+
+instance Show Scope where
+    show (Scope name reqs) = "(\\forall " ++ name ++ ", " ++ show reqs ++ ")"
+
+instance Show Req where
+    show (Req expr x) = showCoreExprGroup expr ++ show x
+    show Demonic = "?Demonic"
+    show Angelic = "?Angelic"
+
