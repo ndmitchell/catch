46c2274bfe0f07e50465540e7af9d0877eda5493
Neil Mitchell <ndmitchell@gmail.com>
2007-02-04 23:14:57 +0000

    Initial make.bat for gadget

---
+++ gadget/make.bat
@@ -0,0 +1,29 @@
+@echo off
+
+if exist C:\Neil\yhc set comp=C:\Neil\yhc
+if exist C:\Documents\Uni\yhc\current set comp=C:\Documents\Uni\yhc\current
+if exist D:\sources\yhc\current set comp=D:\sources\yhc\current
+
+if "%1" == "prof" goto make_prof
+
+set flags=
+set objdir=obj\norm
+set exe=gadget
+set msg=
+
+goto make
+
+:make_prof
+
+set flags=-prof -auto-all
+set objdir=obj\prof
+set exe=gadget_prof
+set msg=Run with gadget_prof +RTS -p
+
+
+:make
+mkdir obj 2> nul
+mkdir %objdir% 2> nul
+ghc %flags% --make Main -o %exe%.exe -odir %objdir% -hidir %objdir% -i%comp%\src\libraries\core -i%comp%\src\libraries\general -i..\proposition
+
+echo Message: %msg%
