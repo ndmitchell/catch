f4bba15ac53f7e1e2c660d40a81359ab18649ce1
Neil Mitchell <ndmitchell@gmail.com>
2007-03-10 23:03:10 +0000

    Write the Analyse.Back module

--- catch_1/Analyse/Back.hs
+++ catch_1/Analyse/Back.hs
@@ -1,14 +1,51 @@
 
-module Analyse.Back where
+module Analyse.Back(backs) where
+
+import Yhc.Core
+import Analyse.Req
+import Analyse.Info
+import Data.Proposition
+import Data.Maybe
+
+
+type Property = CoreFuncName -> Constraint -> IO (PropReq Int)
+
+
+backs :: Property -> PropReq CoreExpr -> IO (PropReq CoreVarName)
+backs prop x = do
+        info <- getInfo
+        res <- fs info x
+        return $ propChange (\(CoreVar i :< k) -> propLit $ i :< k) res
+    where
+        fs info = propMapM (f info)
+        f  info (CoreVar x :< k) | isNothing (var info x) = return $ propLit $ CoreVar x :< k
+        f  info x = back prop x >>= fs info
 
 
 
-back :: Req Expr -> Prop (Req Expr)
-back (Var   x         :< k) = on :< (c |> k)
-    where Just (on, c) = var x
-back (Make  c   xs    :< k) = replaceVars xs (c <| k)
-back (Case  on  alts  :< k) = and [f c e | Alt c vs e <- alts]
-    where f c e = (on :< notin c) || back (e :< k)
-back (Call  fn  xs    :< k) = replaceVars xs (property fn k)
+back :: Property -> Req CoreExpr -> IO (PropReq CoreExpr)
+back prop (CoreVar x :< k) = do
+        info <- getInfo
+        let Just (on, c) = var info x
+        return $ propLit $ on :< ((c |> k) info)
 
-backs :: Prop Req -> Prop (Req VarName)
+back prop (CoreApp (CoreCon c) xs :< k) = do
+        info <- getInfo
+        return $ replaceVars xs ((c <| k) info)
+
+back prop (CoreCase on alts :< k) = do
+        info <- getInfo
+        return $ propAnds [f info (getCtor c) e | (c, e) <- alts]
+    where
+        getCtor (CoreCon c) = c
+        getCtor (CoreApp x _) = getCtor x
+        
+        f info c e = propLit (on :< notin info c) `propOr` propLit (e :< k)
+
+back prop (CoreApp (CoreFun f) xs :< k) = do
+        c <- prop f k
+        return $ replaceVars xs c
+
+
+replaceVars :: [CoreExpr] -> PropReq Int -> PropReq CoreExpr
+replaceVars xs = propChange (\(i:<k) -> propLit $ (xs!!i) :< k)
