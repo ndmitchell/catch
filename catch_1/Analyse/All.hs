2fd760bb3488904eda490853df74b2a11f1973c9
Neil Mitchell <ndmitchell@gmail.com>
2007-03-10 17:41:55 +0000

    Do the initialisation of the info

--- catch_1/Analyse/All.hs
+++ catch_1/Analyse/All.hs
@@ -2,9 +2,15 @@
 module Analyse.All(analyse) where
 
 import Yhc.Core
+import Analyse.Info
 
 
 -- given a logger and the core, do the work
 -- with logger, True = precondition, False = property
 analyse :: (Bool -> String -> IO ()) -> Core -> IO ()
-analyse logger core = error "analyse, to do"
+analyse logger core = do
+    initInfo core
+    error "analyse, to do"
+    termInfo
+
+
