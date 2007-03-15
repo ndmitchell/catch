c6786a9d39d2727530bb673b2878c20ed8cf0115
Neil Mitchell <ndmitchell@gmail.com>
2007-03-15 17:45:49 +0000

    Add groupKey and on to the general list of functions

--- catch_1/General/General.hs
+++ catch_1/General/General.hs
@@ -16,3 +16,12 @@ snub = map head . group . sort
 
 
 traceStrict msg res | length msg >= 0 = trace msg res
+
+
+groupKey :: Ord key => [(key,val)] -> [(key,[val])]
+groupKey = map f . groupBy ((==) `on` fst) . sortBy (compare `on` fst)
+    where f xs = (fst $ head xs, map snd xs)
+
+
+on :: (new -> new -> res) -> (orig -> new) -> (orig -> orig -> res)
+on f g x y = f (g x) (g y)
