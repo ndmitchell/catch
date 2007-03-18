31deb97df289153c11cd9e3e817e5e4718a4ba9e
Neil Mitchell <ndmitchell@gmail.com>
2007-03-18 18:30:55 +0000

    Make Prepare.All export Firstify2

--- catch_1/Prepare/All.hs
+++ catch_1/Prepare/All.hs
@@ -3,6 +3,7 @@ module Prepare.All(
     module Prepare.Compile,
     module Prepare.Overlay,
     module Prepare.Firstify,
+    module Prepare.Firstify2,
     module Prepare.LetElim,
     module Prepare.OneArg,
     module Prepare.UniqueVars,
@@ -12,6 +13,7 @@ module Prepare.All(
 import Prepare.Compile
 import Prepare.Overlay
 import Prepare.Firstify
+import Prepare.Firstify2
 import Prepare.LetElim
 import Prepare.OneArg
 import Prepare.UniqueVars
