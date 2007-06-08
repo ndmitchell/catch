04662eae75e043b86abb360ff41f864594206bd2
Neil Mitchell <ndmitchell@gmail.com>
2007-06-08 17:37:59 +0000

    Lots of fixes, now type checks completely

--- academic/hw2007/Include.hs
+++ academic/hw2007/Include.hs
@@ -1,14 +1,42 @@
 
-import Data.List
+import Data.List hiding (map)
 import Data.Maybe
+import Prelude hiding ((||),(/=),(==),map)
+import System
 
 -- # Ord
+-- # Eq
+-- # alpha
+-- # fn2
+-- # fn
+-- # d
+-- # e
+-- instance Eq RegItem
 
 ss = undefined
 s = undefined
 etc = undefined
 y = undefined
 i = undefined
-e = undefined
 c = undefined
 n = undefined
+x = undefined
+ps = undefined
+xs = undefined
+fn = undefined
+r = undefined
+cs = undefined
+r1 = undefined
+r2 = undefined
+t = undefined
+rec = undefined
+ctor = undefined
+c1 = undefined
+c2 = undefined
+v = undefined
+p1 = undefined
+p2 = undefined
+elipses = undefined
+
+(==>) True False = False
+(==>) _ _ = True
