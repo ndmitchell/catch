4b58f905335b6f66dbf052721820ea8414862350
Neil Mitchell <ndmitchell@gmail.com>
2008-03-19 21:04:05 +0000

    Initial version of the ICFP 2008 paper, just about builds

---
+++ academic/icfp2008/Include.hs
@@ -0,0 +1,40 @@
+
+import Data.List hiding (map)
+import Data.Maybe
+import Prelude hiding ((||),(/=),(==),map)
+import System
+
+-- # Ord
+-- # Eq
+-- # alpha
+-- # fn2
+-- # fn
+-- # d
+-- # e
+-- instance Eq RegItem
+-- instance Eq Val
+
+ss = undefined
+s = undefined
+etc = undefined
+y = undefined
+i = undefined
+c = undefined
+n = undefined
+x = undefined
+ps = undefined
+xs = undefined
+fn = undefined
+r = undefined
+cs = undefined
+r1 = undefined
+r2 = undefined
+t = undefined
+rec = undefined
+ctor = undefined
+c1 = undefined
+c2 = undefined
+v = undefined
+p1 = undefined
+p2 = undefined
+elipses = undefined
