3f3747500f1db6b369a8b25be076a11e7759d1a4
Neil Mitchell <ndmitchell@gmail.com>
2008-06-19 15:55:27 +0000

    Add the initial hw2008 template for Catch

---
+++ academic/hw2008/Include.hs
@@ -0,0 +1,52 @@
+
+import Data.List hiding (map)
+import Data.Maybe
+import Data.Char (isSpace)
+import Data.Array
+import System
+
+-- # Ord
+-- # IO
+-- # Maybe
+-- # Eq
+-- # alpha
+-- # fn2
+-- # fn
+-- # f
+-- # sel
+-- # d
+-- # e
+-- instance Eq RegItem
+-- instance Eq Val
+-- instance Eq (Prop a)
+
+data Rec = Rec0 | RecN | RecN1
+
+ss = undefined
+s = undefined
+etc = undefined
+y = undefined
+i = undefined
+c = undefined
+k = undefined
+alpha = undefined
+beta = undefined
+n = undefined
+x = undefined
+ps = undefined
+xs = undefined
+fn = undefined
+r = undefined
+cs = undefined
+r_1 = undefined
+r_2 = undefined
+t = undefined
+rec = undefined
+ctor = undefined
+c_1 = undefined
+c_2 = undefined
+v = undefined
+p_1 = undefined
+p_2 = undefined
+ellipses = undefined
+f = undefined
